b5c9359f3ee5dd919511a2210de63f7e9a844673
Maurano Lab <maurano@nyu.edu>
2018-10-01 13:52:25 -0400

    Update samplesforTrackhub.R

--- trackhub/samplesforTrackhub.R
+++ trackhub/samplesforTrackhub.R
@@ -5,9 +5,9 @@ library(RColorBrewer)
 library(dplyr)
 option_list = list(
   make_option(c("-f", "--file"), type="character", default=NULL, 
-              help="dataset file name", metavar="character"),
+        help="dataset file name", metavar="character"),
 	make_option(c("-o", "--out"), type="character", default=NULL, 
-              help="output file name", metavar="character")
+        help="output file name", metavar="character")
 ); 
  
 opt_parser = OptionParser(option_list=option_list);
@@ -60,46 +60,46 @@ col<-gsub('#FFFF33','#F0027F',col)
 col<-gsub('#FFFFFF','#BEAED4',col)
 
 Groups<-gsub("-.*",'',bwfiles)
-col<-rep(col,round(length(Groups)/length(col),2))[as.factor(unique(Groups))]        
+col<-rep(col,round(length(Groups)/length(col),2))[as.factor(unique(Groups))]     
 col<-as.data.frame(col,unique(Groups))
 
 data<-data.frame(matrix(ncol=12,nrow=length(bwfiles)))
-colnames(data)<-c('cellType','DSnumber','Replicate','Color','Assay','nonredundant_tags','SPOT','Hotspots','Exclude','Variable','Age', 'Uni')
+colnames(data)<-c('cellType','DSnumber','Replicate','Color','Assay','analyzed_reads','SPOT','Hotspots','Exclude','Variable','Age', 'Uni')
 
 #Change cellType names for Fetal tissues
 
 
 for (i in 1:length(bwfiles)){
-       SampleID<-bwfiles[i]
-       cat(SampleID,'\n')
-       mergedFiles<-list.files(path=paste0(pwd, '/',SampleID), pattern="^makeTracks.*")
-       #cat(mergedFiles, '\n')
-       if (length(mergedFiles)>0)
-              sampleFile<-readLines(paste0(pwd, '/', SampleID,'/',mergedFiles), n=2000)
-              if(tail(sampleFile, 2)[1]=='Done!'){
-                     colGroup<-col2rgb(as.character(col[rownames(col)%in%gsub("-.*",'',bwfiles[i]),][1]))[,1]
-                     data$cellType[i]<-gsub('-.*','',SampleID)
-                     data$DSnumber[i]<-gsub('.hg38','',gsub('.*-','',SampleID))
-                     data$Replicate[i]<-1
-                     data$Color[i]<-paste(colGroup[1],colGroup[2],colGroup[3],sep=',')
-                     data$Assay[i]<-'DNase'
-                     data$nonredundant_tags[i]<-strsplit(sampleFile[grep('Num_analyzed_reads\t',sampleFile)],'\t')[[1]][2]
-                     data$Hotspots[i]<-strsplit(sampleFile[grep('Num_hotspots2\t',sampleFile)],'\t')[[1]][2]
-                     data$SPOT[i]<-strsplit(sampleFile[grep('SPOT2\t',sampleFile)],'\t')[[1]][2]
-                     data$Age[i] <- df$Age[grep(gsub('.hg38','',gsub('.*-','',SampleID)),df$GroupID)]
-                     data$Uni[i] <- df[grep(data$DSnumber[i],df$GroupID),]$Variable[1]#ADDED 20180129
-                     #if there's data in the Variable column, add the information. 
-                     if(is.null(df$Variable)){data$Variable[i]<-" "
-                     } else {data$Variable[i]<-df[grep(data$DSnumber[i],df$GroupID),]$Variable[1]}
-                     #Divide samples based on category
-                     if(length(grep('^f[A-Z]',SampleID))>0){data$Variable[i]<-'Fetal_Roadmap'}
-                     if(length(grep('^CD|^Th|^TH|^hTH|^iTH|^th|^GM1',SampleID))>0){data$Variable[i]<-'Hematopoietic_lineage'}
-                     if(length(grep('^H1|^H7|ES|^H[0-9]|^iPS',SampleID))>0){data$Variable[i]<-'Pluripotent'}
-                     if(length(grep('testis|spinal|Skin|bladder|urothelia|ventriculus|colon|limb|placenta|heart|cortex|kidney|bone|oesteoblast|pancrea|cardia|eye|renal|gonad|muscle|osteo|medulla|brain|ovary|olfact|uteru|fibroblast|lung|tongue|bowel|putamen|esopha|gastro|ammon|derm|nucleus|gast|glom|gyrus|thyroid|adipo|neuron|prostate|intest|medull',
-                     SampleID[grep('^f[A-Z]',SampleID,invert=T)],ignore.case=T,invert=F))>0){data$Variable[i]<-'Tissues'}
-                     #rm(sampleFile)
-                     #gc()
-              }
+    SampleID<-bwfiles[i]
+    cat(SampleID,'\n')
+    mergedFiles<-list.files(path=paste0(pwd, '/',SampleID), pattern="^makeTracks.*")
+    #cat(mergedFiles, '\n')
+    if (length(mergedFiles)>0)
+        sampleFile<-readLines(paste0(pwd, '/', SampleID,'/',mergedFiles), n=2000)
+        if(tail(sampleFile, 2)[1]=='Done!'){
+            colGroup<-col2rgb(as.character(col[rownames(col)%in%gsub("-.*",'',bwfiles[i]),][1]))[,1]
+            data$cellType[i]<-gsub('-.*','',SampleID)
+            data$DSnumber[i]<-gsub('.hg38','',gsub('.*-','',SampleID))
+            data$Replicate[i]<-1
+            data$Color[i]<-paste(colGroup[1],colGroup[2],colGroup[3],sep=',')
+            data$Assay[i]<-'DNase'
+            data$analyzed_reads[i]<-strsplit(sampleFile[grep('Num_analyzed_reads\t',sampleFile)],'\t')[[1]][2]
+            data$Hotspots[i]<-strsplit(sampleFile[grep('Num_hotspots2\t',sampleFile)],'\t')[[1]][2]
+            data$SPOT[i]<-strsplit(sampleFile[grep('SPOT2\t',sampleFile)],'\t')[[1]][2]
+            data$Age[i] <- df$Age[grep(gsub('.hg38','',gsub('.*-','',SampleID)),df$GroupID)]
+            data$Uni[i] <- df[grep(data$DSnumber[i],df$GroupID),]$Variable[1]#ADDED 20180129
+            #if there's data in the Variable column, add the information. 
+            if(is.null(df$Variable)){data$Variable[i]<-" "
+            } else {data$Variable[i]<-df[grep(data$DSnumber[i],df$GroupID),]$Variable[1]}
+            #Divide samples based on category
+            if(length(grep('^f[A-Z]',SampleID))>0){data$Variable[i]<-'Fetal_Roadmap'}
+            if(length(grep('^CD|^Th|^TH|^hTH|^hTR|^iTH|^th|^GM1|^GM06990|^Jurkat',SampleID))>0){data$Variable[i]<-'Hematopoietic_lineage'}
+            if(length(grep('^H1|^H7|ES|^H[0-9]|^iPS',SampleID))>0){data$Variable[i]<-'Pluripotent'}
+            if(length(grep('testis|spinal|Skin|bladder|urothelia|ventriculus|colon|limb|placenta|heart|cortex|kidney|bone|pancrea|cardia|eye|renal|gonad|muscle|osteo|medulla|brain|ovary|olfact|uteru|fibroblast|lung|tongue|bowel|putamen|esopha|gastro|ammon|derm|nucleus|gast|glom|gyrus|thyroid|adipo|neuron|prostate|intest|medull|[Ll]iver|aggregated_lymphoid_nodule|aorta|artery|psoas|stomach|testes|tibial_artery|vagina|omental_fat_depot|fetal_umbilical_cord|pons|medial_popliteal_nerve|globus|Spleen',
+            SampleID[grep('^f[A-Z]',SampleID,invert=T)],ignore.case=T,invert=F))>0){data$Variable[i]<-'Tissues'}
+            #rm(sampleFile)
+            #gc()
+        }
 }
 
 
@@ -111,24 +111,24 @@ data$Age[grep('^8 ',data$Age)] <- '08 weeks'
 
 ##Rename the fetal tissues 
 #fetalRename <- data[grep('day|week',data$Age),] %>% 
-#       filter(!grepl('^f',cellType)) %>%
-#       filter(!grepl('AG04449|AG04450|IMR_90',cellType)) %>%
-#       select(DSnumber)
-#       
+#    filter(!grepl('^f',cellType)) %>%
+#    filter(!grepl('AG04449|AG04450|IMR_90',cellType)) %>%
+#    select(DSnumber)
+#    
 #library(Hmisc)
 #data[data$DSnumber%in%fetalRename$DSnumber,]$cellType <- gsub('^','f',capitalize(data[data$DSnumber%in%fetalRename$DSnumber,]$cellType))
 #data[data$DSnumber%in%fetalRename$DSnumber,]$Variable <- 'Fetal_roadmap'
 
 
-#Add replicate numbers based on highest number of non redundant tags
+#Add replicate numbers based on highest number of non redundant reads
 data<-data[!is.na(data$cellType),]
 Replicates<-unique(subset(data,select=c(cellType,Variable)))
 Replicates$cellType <- gsub('_L$|_R$','',Replicates$cellType)
 for (i in 1:nrow(Replicates)){
-       #cat(Replicates$cellType[i], Replicates$Variable[i])
-       data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i] & data$Variable==Replicates$Variable[i],]$Replicate <- rank(-as.numeric(data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i] & data$Variable==Replicates$Variable[i],]$nonredundant_tags))
-       #Fix color for Left and right tissues
-       data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i] & data$Variable==Replicates$Variable[i],]$Color <- names(tail(table(data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i],]$Color),1))
+    #cat(Replicates$cellType[i], Replicates$Variable[i])
+    data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i] & data$Variable==Replicates$Variable[i],]$Replicate <- rank(-as.numeric(data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i] & data$Variable==Replicates$Variable[i],]$analyzed_reads))
+    #Fix color for Left and right tissues
+    data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i] & data$Variable==Replicates$Variable[i],]$Color <- names(tail(table(data[gsub('_L$|_R$','',data$cellType)==Replicates$cellType[i],]$Color),1))
 }
 
 data$Variable[data$Variable=='UMass']<-'UW'
@@ -140,15 +140,15 @@ dataRep$cellType <- gsub('_L$|_R$','',dataRep$cellType)
 dataRep <- split(dataRep, dataRep$cellType)
 #repDF <- dataRep[[3]]
 dataReplist <- lapply(dataRep, function(repDF) {
-       head(repDF)
-       #repDF$Replicate <- NA
-       if ( nrow(repDF[grep('UW', repDF$Uni),]) >0 ) {
-               repDF[grep('UW', repDF$Uni),]$Replicate <- rank(-as.numeric(repDF[grep('UW', repDF$Uni),]$nonredundant_tags))
-               repDF[grep('UW', repDF$Uni, invert=T),]$Replicate <- rank(-as.numeric(repDF[grep('UW', repDF$Uni , invert=T),]$nonredundant_tags)) + nrow(repDF[grep('UW', repDF$Uni),])
-       } else { repDF$Replicate <- rank(-as.numeric(repDF$nonredundant_tags))
-       }
-       repDF
-})               
+    head(repDF)
+    #repDF$Replicate <- NA
+    if ( nrow(repDF[grep('UW', repDF$Uni),]) >0 ) {
+         repDF[grep('UW', repDF$Uni),]$Replicate <- rank(-as.numeric(repDF[grep('UW', repDF$Uni),]$analyzed_reads))
+         repDF[grep('UW', repDF$Uni, invert=T),]$Replicate <- rank(-as.numeric(repDF[grep('UW', repDF$Uni , invert=T),]$analyzed_reads)) + nrow(repDF[grep('UW', repDF$Uni),])
+    } else { repDF$Replicate <- rank(-as.numeric(repDF$analyzed_reads))
+    }
+    repDF
+})         
 
 data <- do.call(rbind, dataReplist)
 
