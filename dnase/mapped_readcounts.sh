69c1254da1df1ff234df874ed594868194bd593b
Maurano Lab <maurano@nyu.edu>
2018-11-02 14:52:25 -0400

    Create mapped_readcounts.sh

---
+++ dnase/mapped_readcounts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eu -o pipefail
+
+grep -h -A 11 "Num_sequenced_reads" *BS*/analysis.*.o* | awk 'BEGIN {skip=0} $0=="--" {skip=0;next} $0=="" {skip=1;next} skip==0' | grep -v -e "syntax[ _]error" | grep -v "Num_SE_" | grep -v "Prop_SE_" | 
+#awk '$1!="$1!="Num_hotspots" && SPOT"' |
+perl -p -e's/[ \t]\(?([\d\.]+)\%\)?/\t$1/g;' -e's/ /_/g;' -e's/\-BS/\tBS/g;' | 
+#perl -pe 's/^(Genomic_coverage\t[^\t]+\t)/\1\t/g;' |
+awk -F "\t" 'BEGIN {OFS="\t"; print "Label", "Num", "Pct", "Cell_type", "BS", "Genome"} {if(NF==4) {$5="NA"; $6="NA"} else if (NF==5) {$6="NA"} print}' | cut -f1-6 > /tmp/t.txt
+
+R --quiet --no-save << 'EOF'
+data <- read("/tmp/t.txt", header=T)
+data <- reshape(data, idvar=c("Genome", "Cell_type", "BS"), timevar="Label", direction="wide")
+data <- data[,!apply(data, MARGIN=2, FUN=function(x) {all(is.na(x))})]
+colnames(data) <- gsub("^Num\\.Num_", "Num_", colnames(data))
+colnames(data) <- gsub("^Pct\\.Num_", "Prop_", colnames(data))
+#gives error for columns with NA
+data[,grepl("^Prop_", colnames(data))] <- apply(data[,grepl("^Prop.", colnames(data))], MARGIN=c(1,2), FUN=function(x) {as.numeric(x)/100})
+colnames(data) <- gsub("^Num\\.SPOT(2?)", "SPOT\\1", colnames(data))
+data <- data[order(data[,"BS"]),]
+print.data.frame(data, row.names=F)
+write.table(data, row.names=F, col.names=T, quote=F, file="readcounts.summary.txt", append=F, sep="\t")
+EOF
+
+
+echo
+echo "Done!!!"
+date
