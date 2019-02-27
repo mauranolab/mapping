21a27250b4c12e46a0e8909f74bc2aee86e7a384
John Cadley <45205158+cadley-nyulangone@users.noreply.github.com>
2019-02-27 09:10:55 -0500

    added two lines

--- dnase/trackhub/samplesforTrackhub.R
+++ dnase/trackhub/samplesforTrackhub.R
@@ -66,6 +66,9 @@ if(project=="CEGS") {
 	# Loop over all the flowcells to find sample directories.
 	flowcell_dates = list()
 	for(flowcell in list.dirs(path=pwd, full.names=F, recursive = F)) {
+	    if(flowcell=='src') next;
+		if(flowcell=='trackhub') next;
+
 		# To temporarily skip flowcell directories which are incomplete.
 		# if(flowcell=='FCH2NNMBBXY') next;
 		
