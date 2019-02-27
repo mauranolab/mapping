06cfde33b471a977671e4bf03784c4d4d518418b
mattmaurano <maurano@nyu.edu>
2019-02-27 16:30:56 -0500

    clean up directories with trailing /

--- transposon/submitBCmerge.sh
+++ transposon/submitBCmerge.sh
@@ -9,7 +9,7 @@ sample=$1
 shift
 indivsamples=$@
 
-bcfiles=`echo $indivsamples | awk 'BEGIN {ORS=" "} {for(i=1; i<=NF; i++) {split($i, path, "/"); print $i "/" path[length(path)] ".barcodes.preFilter.txt" }}'`
+bcfiles=`echo $indivsamples | perl -pe 's/\/+$//g;' -e 's/\/+( +)/\1/g;' | awk 'BEGIN {ORS=" "} {for(i=1; i<=NF; i++) {split($i, path, "/"); print $i "/" path[length(path)] ".barcodes.preFilter.txt" }}'`
 echo -e "Will merge barcode files: ${bcfiles}\n"
 qsub -S /bin/bash -j y --qos=full -N ${sample} -b y <<EOF
 set -eu -o pipefail
