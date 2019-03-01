e660645795c267f95abf4f6db935cfbcfc4df4a5
mattmaurano <maurano@nyu.edu>
2019-03-01 11:08:33 -0500

    mappable coverage was only one chrom, clean up treatment of dup reads

--- dnase/analyzeCapture.sh
+++ dnase/analyzeCapture.sh
@@ -27,7 +27,7 @@ esac
 
 
 case "${bait}" in
-HPRT_bait)
+HPRT1_bait)
     grep -w "HPRT1_assembly" /vol/cegs/sequences/HPRT1/HPRT1_assembly.hg38.bed | bedops -m - > $TMPDIR/target.bed
     ;;
 RnHoxa_bait)
@@ -46,37 +46,40 @@ if [ `cut -f1 $TMPDIR/target.bed | uniq | wc -l` -ne 1 ]; then
 fi
 chrom=`cut -f1 $TMPDIR/target.bed | uniq`
 
+echo "Analyzing ${bait} bait mapped to ${mappedgenome}"
+
 
 #Summary of on-target coverage
 #NB genomecov is over all chrom.sizes -- right way?
 
-echo -e "Sample\tReads_analyzed\tReads_on_target\tcov.wholegenome\tcov.mappablegenome\tcov.mean\tcov.sd\tcov.5th.pctile\tcov.median"
-for f in `find ${dirs} -name "*.${mappedgenome}.coverage.binned.starch"`; do
-    base=`basename ${f} .${mappedgenome}.coverage.binned.starch`
+echo -e "Sample\tNonredundant_reads_analyzed\tNonredundant_reads_on_target\tcov.wholegenome\tcov.mappablegenome\tcov.mean\tcov.sd\tcov.5th.pctile\tcov.median"
+for covfile in `find ${dirs} -name "*.${mappedgenome}.coverage.binned.starch"`; do
+    base=`basename ${covfile} .${mappedgenome}.coverage.binned.starch`
     echo -n -e "${base}\t"
     
-    readsfile=`echo ${f} | perl -pe 's/\.coverage.binned.starch$/.reads.starch/g;'`
+    readsfile=`echo ${covfile} | perl -pe 's/\.coverage.binned.starch$/.reads.starch/g;'`
+    bamfile=`echo ${covfile} | perl -pe 's/\.coverage.binned.starch$/.bam/g;'`
     
-    #Reads_analyzed
-    unstarch --elements ${readsfile} | perl -pe 's/\n/\t/g;'
+    #Nonredundant_reads_analyzed
+    samtools view -F 1536 -c ${bamfile} | perl -pe 's/\n/\t/g;'
     
-    #Reads_on_target
-    bedops --chrom ${chrom} -e -1 ${readsfile} $TMPDIR/target.bed | wc -l | perl -pe 's/\n/\t/g;'
+    #Nonredundant_reads_on_target
+    bedops --chrom ${chrom} -e -1 ${readsfile} $TMPDIR/target.bed | awk -F "\t" 'BEGIN {OFS="\t"} !and($7, 1024)' | wc -l | perl -pe 's/\n/\t/g;'
     
-#    analysisFile=`dirname ${f} | xargs -I {} find {} -name "analysis.*.${mappedgenome}.o*"`
+#    analysisFile=`dirname ${covfile} | xargs -I {} find {} -name "analysis.*.${mappedgenome}.o*"`
 #    genomecov=`awk -F "\t" 'BEGIN {OFS="\t"} $1=="Genomic_coverage" {print $2; exit}' ${analysisFile}`
 #    echo -n -e "${genomecov}\t"
     
     #cov.wholegenome
-    unstarch ${f} | awk -F "\t" 'BEGIN {OFS="\t"; sum=0; count=0} {sum+=$5; count+=1} END {if(count==0) {print "NA"} else {print sum/count}}' | perl -pe 's/\n/\t/g;'
+    unstarch ${covfile} | awk -F "\t" 'BEGIN {OFS="\t"; sum=0; count=0} {sum+=$5; count+=1} END {if(count==0) {print "NA"} else {print sum/count}}' | perl -pe 's/\n/\t/g;'
     
     #cov.mappablegenome
-    unstarch ${chrom} ${f} | bedops --chrom ${chrom} -e -1 - ${mappableFile} | awk -F "\t" 'BEGIN {OFS="\t"; sum=0; count=0} {sum+=$5; count+=1} END {if(count==0) {print "NA"} else {print sum/count}}' | perl -pe 's/\n/\t/g;'
+    unstarch ${covfile} | bedops -e -1 - ${mappableFile} | awk -F "\t" 'BEGIN {OFS="\t"; sum=0; count=0} {sum+=$5; count+=1} END {if(count==0) {print "NA"} else {print sum/count}}' | perl -pe 's/\n/\t/g;'
     
     #cov.mean, cov.sd, cov.5th.pctile, cov.median
-    #bedmap --chrom ${chrom} --faster --delim "\t" --bp-ovr 1 --prec 2 --mean --stdev --kth 0.05 --median $TMPDIR/target.bed ${f}
+    #bedmap --chrom ${chrom} --faster --delim "\t" --bp-ovr 1 --prec 2 --mean --stdev --kth 0.05 --median $TMPDIR/target.bed ${covfile}
     #Use mlr to handle discontinuous target regions
-    bedmap --chrom ${chrom} --faster --delim "\n" --multidelim "\n" --bp-ovr 1 --prec 3 --echo-map-score $TMPDIR/target.bed ${f} | mlr  --tsv --implicit-csv-header --headerless-csv-output stats1 -a mean,stddev,p5,p50 -f 1
+    bedmap --chrom ${chrom} --faster --delim "\n" --multidelim "\n" --bp-ovr 1 --prec 3 --echo-map-score $TMPDIR/target.bed ${covfile} | mlr  --tsv --implicit-csv-header --headerless-csv-output stats1 -a mean,stddev,p5,p50 -f 1
 done
 
 
