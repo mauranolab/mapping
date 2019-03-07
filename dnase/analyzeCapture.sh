351e038f79741bfd98a3acd8ca346809157d0443
mattmaurano <maurano@nyu.edu>
2019-03-07 13:52:36 -0500

    new file locations, output total seq reads

--- dnase/analyzeCapture.sh
+++ dnase/analyzeCapture.sh
@@ -28,10 +28,10 @@ esac
 
 case "${bait}" in
 HPRT1_bait)
-    grep -w "HPRT1_assembly" /vol/cegs/sequences/HPRT1/HPRT1_assembly.hg38.bed | bedops -m - > $TMPDIR/target.bed
+    grep -w "HPRT1_assembly" /vol/cegs/sequences/hg38/HPRT1/HPRT1_assembly.bed | bedops -m - > $TMPDIR/target.bed
     ;;
 RnHoxa_bait)
-    grep -w "RnHoxa_assembly" /vol/cegs/sequences/RnHoxa/Hoxa_rat_assembly.rn6.bed | bedops -m - > $TMPDIR/target.bed
+    grep -w "RnHoxa_assembly" /vol/cegs/sequences/rn6/RnHoxa/RnHoxa_assembly.bed | bedops -m - > $TMPDIR/target.bed
     ;;
 *)
     echo "ERROR: Don't recognize bait ${bait}";
@@ -52,7 +52,7 @@ echo "Analyzing ${bait} bait mapped to ${mappedgenome}"
 #Summary of on-target coverage
 #NB genomecov is over all chrom.sizes -- right way?
 
-echo -e "Sample\tNonredundant_reads_analyzed\tNonredundant_reads_on_target\tcov.wholegenome\tcov.mappablegenome\tcov.mean\tcov.sd\tcov.5th.pctile\tcov.median"
+echo -e "Sample\tNum_sequenced_reads\tNonredundant_reads_analyzed\tNonredundant_reads_on_target\tcov.wholegenome\tcov.mappablegenome\tcov.mean\tcov.sd\tcov.5th.pctile\tcov.median"
 for covfile in `find ${dirs} -name "*.${mappedgenome}.coverage.binned.starch"`; do
     base=`basename ${covfile} .${mappedgenome}.coverage.binned.starch`
     echo -n -e "${base}\t"
@@ -60,15 +60,15 @@ for covfile in `find ${dirs} -name "*.${mappedgenome}.coverage.binned.starch"`; 
     readsfile=`echo ${covfile} | perl -pe 's/\.coverage.binned.starch$/.reads.starch/g;'`
     bamfile=`echo ${covfile} | perl -pe 's/\.coverage.binned.starch$/.bam/g;'`
     
+    analysisFile=`dirname ${covfile} | xargs -I {} find {} -name "analysis.*.${mappedgenome}.o*"`
+    sequencedReads=`awk -F "\t" 'BEGIN {OFS="\t"} $1=="Num_sequenced_reads" {print $2; exit}' ${analysisFile}`
+    echo -n -e "${sequencedReads}\t"
+    
     #Nonredundant_reads_analyzed
     samtools view -F 1536 -c ${bamfile} | perl -pe 's/\n/\t/g;'
     
     #Nonredundant_reads_on_target
     bedops --chrom ${chrom} -e -1 ${readsfile} $TMPDIR/target.bed | awk -F "\t" 'BEGIN {OFS="\t"} !and($7, 1024)' | wc -l | perl -pe 's/\n/\t/g;'
-    
-#    analysisFile=`dirname ${covfile} | xargs -I {} find {} -name "analysis.*.${mappedgenome}.o*"`
-#    genomecov=`awk -F "\t" 'BEGIN {OFS="\t"} $1=="Genomic_coverage" {print $2; exit}' ${analysisFile}`
-#    echo -n -e "${genomecov}\t"
     
     #cov.wholegenome
     unstarch ${covfile} | awk -F "\t" 'BEGIN {OFS="\t"; sum=0; count=0} {sum+=$5; count+=1} END {if(count==0) {print "NA"} else {print sum/count}}' | perl -pe 's/\n/\t/g;'
