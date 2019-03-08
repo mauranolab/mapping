f85f3a12871ce56b3c5114002bd3a398aec7373c
mattmaurano <maurano@nyu.edu>
2019-03-08 11:09:59 -0500

    refactor use common submit script for dna/rna/ipcr. requires change in command line arguments. separate merge/analysis jobs in preparation for allowing submission of intermediate jobs

--- transposon/submitMerge.sh
+++ transposon/submitMerge.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 #Based on /home/maagj01/scratch/transposon/submitMerged.sh
+#TODO merge with submitMerge.sh a la dnase pipeline -- main issue is whether to remove files or not
+
 
 #Limit thread usage by python processes using OPENBLAS (esp. scipy). Set here and will be inherited by spawned jobs
 #https://stackoverflow.com/questions/51256738/multiple-instances-of-python-running-simultaneously-limited-to-35
@@ -16,31 +18,50 @@ sample=$1
 shift
 indivsamples=$@
 
+OUTDIR=${sample}
+
 bcfiles=`echo ${indivsamples} | perl -pe 's/\/+$//g;' -e 's/\/+( +)/\1/g;' | awk 'BEGIN {ORS=" "} {for(i=1; i<=NF; i++) {split($i, path, "/"); print $i "/" path[length(path)] ".barcodes.preFilter.txt.gz" }}'`
 echo -e "Will merge barcode files: ${bcfiles}\n"
 
 if [[ ${indivsamples} =~ _iPCR ]]; then
-    mergeIntegrations=1
+    sampleType = "iPCR"
     minReadCutoff=2
     bamfiles=`echo ${indivsamples} | perl -pe 's/\/+$//g;' -e 's/\/+( +)/\1/g;' | awk 'BEGIN {ORS=" "} {for(i=1; i<=NF; i++) {split($i, path, "/"); print $i "/" path[length(path)] ".bam" }}'`
     echo -e "Will merge bam files: ${bamfiles}\n"
 else
-    mergeIntegrations=0
+    #As long as it's not iPCR
+    sampleType == "DNA"
     minReadCutoff=10
     bamfiles=""
 fi
 
-qsub -S /bin/bash -j y -N ${sample} -b y <<EOF
+
+cat <<EOF | qsub -S /bin/bash -j y -b y -N ${sample} -o ${sample} -terse > sgeid.merge.${sample}
 set -eu -o pipefail
 mkdir -p ${sample}
 echo "Merging barcodes"
-zcat ${bcfiles} | pigz -p ${NSLOTS} -c -9 > ${sample}/${sample}.barcodes.preFilter.txt.gz
+zcat -f ${bcfiles} | pigz -p ${NSLOTS} -c -9 > ${OUTDIR}/${sample}.barcodes.preFilter.txt.gz
+
+if [[ ${sampleType} == "iPCR" ]]; then
+    echo "Merging bam files"
+    if [[ `echo ${bamfiles} | wc | awk '{print $2}'` -gt 1 ]]; then 
+        samtools merge -f -l 9 $OUTDIR/${sample}.bam ${bamfiles}
+    else 
+        cp ${bamfiles} $OUTDIR/${sample}.bam
+    fi
+    #TODO sort in mapIntegrations.sh?
+    #samtools index $OUTDIR/${sample}.bam
+fi
+EOF
+
+
+#-o ${sample} breaks Jesper's Flowcell_Info.sh
+cat <<EOF | qsub -S /bin/bash -j y -b y -N ${sample} -terse -hold_jid `cat sgeid.merge.${sample}` | perl -pe 's/[^\d].+$//g;'  #> sgeid.analysis
+set -eu -o pipefail
 ${src}/analyzeBCcounts.sh ${minReadCutoff} ${sample}
 
-if [ ${mergeIntegrations} -eq 1 ]; then
-    echo
-    echo "Merging integration sites"
-    samtools merge -f -l 9 $sample/${sample}.bam ${bamfiles}
+if [[ ${sampleType} == "iPCR" ]]; then
     ${src}/analyzeIntegrations.sh ${sample}
 fi
 EOF
+rm -f sgeid.merge.${sample}
