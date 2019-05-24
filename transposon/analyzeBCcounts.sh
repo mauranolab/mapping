a2cc2bb7fee73cad3251840f4bf10a790240e5c9
mattmaurano <maurano@nyu.edu>
2019-05-24 10:42:19 -0400

    change reporting to use UMI-corrected counts

--- transposon/analyzeBCcounts.sh
+++ transposon/analyzeBCcounts.sh
@@ -29,6 +29,10 @@ fi
 
 pigz -p ${NSLOTS} -c -9 ${TMPDIR}/${sample}.barcodes.txt > ${OUTDIR}/${sample}.barcodes.txt.gz
 
+#Empty BCs haven't been filtered yet for non-UMI data
+zcat -f ${OUTDIR}/${sample}.barcodes.txt.gz | awk -F "\t" '$1!=""' | cut -f1 | sort | uniq -c | awk 'BEGIN {OFS="\t"} {print $2, $1}' > ${OUTDIR}/${sample}.barcode.counts.txt
+
+
 ###Overall summary statistics
 echo
 numTotalReads=`zcat -f ${OUTDIR}/${sample}.barcodes.txt.gz | wc -l`
@@ -67,6 +71,8 @@ if [ "${minUMILength}" -ge 5 ]; then
     awk 'BEGIN {OFS="\t"} {print $2, $3, $1}' |
     pigz -p ${NSLOTS} -c -9 > ${OUTDIR}/${sample}.barcode.counts.withUMI.txt.gz
     zcat -f ${OUTDIR}/${sample}.barcode.counts.withUMI.txt.gz | cut -f1 | sort | uniq -c | awk 'BEGIN {OFS="\t"} {print $2, $1}' > ${OUTDIR}/${sample}.barcode.counts.UMI_corrected.txt
+    countfile="${OUTDIR}/${sample}.barcode.counts.UMI_corrected.txt"
+    
     echo -n -e "${sample}\tNumber of unique barcodes+UMI\t"
     #TODO move to extract.py
     zcat -f ${OUTDIR}/${sample}.barcode.counts.withUMI.txt.gz | wc -l
@@ -80,46 +86,42 @@ if [ "${minUMILength}" -ge 5 ]; then
     #No more empty BCs are present so no need to check
     zcat -f ${OUTDIR}/${sample}.barcode.counts.withUMI.txt.gz | cut -f2 |
     awk '{print length($0)}' | sort -g | uniq -c | sort -k2,2 | awk '$2!=0'
-    
-    #TODO why bother with the bcfile variable since it's the same in both cases?
-    bcfile="${OUTDIR}/${sample}.barcodes.txt.gz"
 else
     echo
     echo "No UMIs found"
     
-    bcfile="${OUTDIR}/${sample}.barcodes.txt.gz"
+    countfile="${sample}.barcode.counts.txt"
 fi
 
 echo
 echo "Barcode counts"
-#Empty BCs haven't been filtered yet for non-UMI data
-zcat -f ${bcfile} | awk -F "\t" '$1!=""' | cut -f1 | sort | uniq -c | awk 'BEGIN {OFS="\t"} {print $2, $1}' > ${OUTDIR}/${sample}.barcode.counts.txt
 
 echo -n -e "${sample}\tNumber of unique barcodes\t"
 #TODO move to extract.py
-cat ${OUTDIR}/${sample}.barcode.counts.txt | wc -l
+cat ${countfile} | wc -l
 
 echo -n -e "${sample}\tNumber of unique barcodes passing minimum read cutoff\t"
 #TODO move to extract.py
-cat ${OUTDIR}/${sample}.barcode.counts.txt | awk -v minReadCutoff=${minReadCutoff} -F "\t" 'BEGIN {OFS="\t"} $2>=minReadCutoff' | wc -l
+cat ${countfile} | awk -v minReadCutoff=${minReadCutoff} -F "\t" 'BEGIN {OFS="\t"} $2>=minReadCutoff' | wc -l
 
 echo -n -e "${sample}\tNumber of analyzed reads\t"
-cat ${OUTDIR}/${sample}.barcode.counts.txt | awk -v minReadCutoff=${minReadCutoff} -F "\t" 'BEGIN {OFS="\t"; sum=0; total=0} $2>=minReadCutoff {sum+=$2} {total+=$2} END {print sum}'
+cat ${countfile} | awk -v minReadCutoff=${minReadCutoff} -F "\t" 'BEGIN {OFS="\t"; sum=0; total=0} $2>=minReadCutoff {sum+=$2} {total+=$2} END {print sum}'
 
 
 echo
 echo "Histogram of number of reads per barcode"
-cat ${OUTDIR}/${sample}.barcode.counts.txt | cut -f2 | awk -v cutoff=10 '{if($0>=cutoff) {print cutoff "+"} else {print}}' | sort -g | uniq -c | sort -k2,2g
+cat ${countfile} | cut -f2 | awk -v cutoff=10 '{if($0>=cutoff) {print cutoff "+"} else {print}}' | sort -g | uniq -c | sort -k2,2g
 
 
 echo
 echo "Barcode lengths"
-cut -f1 ${OUTDIR}/${sample}.barcode.counts.txt |
+cut -f1 ${countfile} |
 awk '{print length($0)}' | sort -g | uniq -c | sort -k2,2 | awk '$2!=0'
 
 
 echo
 echo "Saturation curves"
+#BUGBUG does not consider UMIs for thresholding
 #NB hardcoded limit of 120M reads
 for i in `echo {10000,50000,100000,250000,500000,1000000,2000000,3000000,4000000,5000000,10000000,15000000,20000000,25000000,30000000,35000000,40000000,45000000,50000000,55000000,60000000,70000000,80000000,90000000,100000000,110000000,120000000} | tr ' ' '\n' | gawk -v subset=${numTotalReads} '$1<=subset'`; do 
     if [ "${minUMILength}" -ge 5 ]; then
