da14a772917b46f91f8fe8e9d4e619f97056cb71
cadley-nyulangone <45205158+cadley-nyulangone@users.noreply.github.com>
2019-07-22 16:19:32 -0400

    Modify Description HTML files (#57)
    * Modify Description HTML files
    
    Use a single html file for both the hub and cegsvectors html description files.
    
    fixes #56

--- dnase/trackhub/update_tracks.bash
+++ dnase/trackhub/update_tracks.bash
@@ -248,7 +248,7 @@ echo "shortLabel ${short_label}" >> "${hub_target}/hub.txt"
 echo "longLabel ${long_label}" >> "${hub_target}/hub.txt"
 echo "genomesFile genomes.txt" >> "${hub_target}/hub.txt"
 echo "email cadley.nyulangone@gmail.com" >> "${hub_target}/hub.txt"
-echo "descriptionUrl hub_description.html" >> "${hub_target}/hub.txt"
+echo "descriptionUrl description.html" >> "${hub_target}/hub.txt"
 
 cd ${path_to_main_driver_script}
 
@@ -272,8 +272,7 @@ fi
 #########################################################
 
 time_stamp=$(date +"%m-%d-%y %T")
-echo "<pre>Hub was constructed at: ${time_stamp} </pre>" >> "${hub_target}/genomes_description.html"
-echo "<pre>Hub was constructed at: ${time_stamp} </pre>" >> "${hub_target}/hub_description.html"
+echo "<pre>Hub was constructed at: ${time_stamp} </pre>" >> "${hub_target}/description.html"
 
 ######################################################################################
 # Provide a README with the hub link.
