9ca24a7b75589c63906e1a19cfc575e1cc6808d9
mattmaurano <maurano@nyu.edu>
2019-08-07 10:52:12 -0400

    check whether the number of cycles programmed matches kit capacity

--- flowcells/demuxFC.sh
+++ flowcells/demuxFC.sh
@@ -28,6 +28,7 @@ fi
 #-r 0.25 -w 0.25 -p 0.875
 #I bumped up based on observed CPU usage
 #TODO set fastq-compression-level 1 if we split everything afterwards
+#Info on --use-bases-mask https://www.biostars.org/p/344768/
 bcl2fastq --fastq-compression-level 9 --no-lane-splitting --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --barcode-mismatches 0 -r $((${NSLOTS}/4)) -w $((${NSLOTS}/4)) -p $((${NSLOTS}/7*8)) --output-dir ../../fastq/${fc} ${userDemuxOptions} > bcl2fastq.log 2>&1
 
 
