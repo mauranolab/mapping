ac733eb86a4c9d2d3f18c480a77df2cd3480499a
John Cadley <45205158+cadley-nyulangone@users.noreply.github.com>
2019-09-05 15:40:09 -0400

    Added link to clarification of picard sort order
    In comments.

--- dnase/bamintersect/sort_bamintersect.sh
+++ dnase/bamintersect/sort_bamintersect.sh
@@ -28,6 +28,7 @@ BAM_OUT="${sampleOutdir}/bams/${sample_name}.${chrom}.${BAM_N}.bam"
 input_to_samtools3=${input_to_samtools//|/ }
 
 ## Sort bam file by read name.  picard does it lexigraphically.  samtools does it "naturally".
+## See: https://github.com/samtools/hts-specs/pull/361 for clarification and pointers to background about this.
 samtools view -h -b -f ${BAM_K} -F ${BAM_E} ${BAM} ${input_to_samtools3} | \
 java -XX:ParallelGCThreads=2 -Dpicard.useLegacyParser=false -jar $PICARDPATH/picard.jar SortSam \
      -I=/dev/stdin \
