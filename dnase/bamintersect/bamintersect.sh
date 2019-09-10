b6bbd155aea08b1def55379a0598cdff88ddd09b
cadley-nyulangone <45205158+cadley-nyulangone@users.noreply.github.com>
2019-09-10 16:25:37 -0400

    Restructures use of temp directories (#69)
    * Restructures use of temp directories
    
    Uses a new tmp dir to hold files which need to be persistent
    during all the sbatch executions.
    
    * Changed names of output and temp directories
    
    fixes #69

--- dnase/bamintersect/bamintersect.sh
+++ dnase/bamintersect/bamintersect.sh
@@ -2,26 +2,23 @@
 set -eu -o pipefail
 
 ########################################################
-## sampleOutdir is passed in via an sbatch export
 ## src is passed in via an sbatch export
 ## reads_match is passed in via an sbatch export
 ## make_csv is passed in via an sbatch export
+## OUTDIR_bams is passed in via an sbatch export (contains output of sort_bamintersect.sh)
 ########################################################
 
 echo "SLURM_ARRAY_JOB_ID=$SLURM_ARRAY_JOB_ID"
 echo "SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID"
-echo "sampleOutdir is: ${sampleOutdir}"   # Passed in via sbatch export
 echo
 
 ## The "|| true" prevents the SIGPIPE signal problem. It's only needed when set -eo pipefail is enabled.
-bam_intersect_data=$(tail -n+${SLURM_ARRAY_TASK_ID} "${sampleOutdir}/bams/array_list" | head -n 1) || true
+bam_intersect_data=$(tail -n+${SLURM_ARRAY_TASK_ID} "${OUTDIR_bams}/inputs.array_list.txt" | head -n 1) || true
 
 read -r bam1 bam2 outdir <<< ${bam_intersect_data}
 echo ${bam1}
 echo ${bam2}
-echo ${outdir}
-
-mkdir ${outdir}    ## "${TEMP_DIR_CL1}/${BASE1}___${BASE2}" 
+echo ${outdir}      # "${OUTDIR_bamintersectPyOut}/${BASE1}___${BASE2}"
 
 ####################################################################################################################
 ## reads_match=True means that we are looking to pair up read1's with read1's, and read2's with read2's.
@@ -41,16 +38,19 @@ else
     make_csv_flag="--make_csv"
 fi
 
-"${src}/bamintersect.py" --bam1 ${bam1} --bam2 ${bam2} --outdir ${outdir} ${same} ${make_csv_flag}
+TMPDIR=`mktemp -d`    # A working directory for bamintersect.py, which is part of an array job.
+"${src}/bamintersect.py" --bam1 ${bam1} --bam2 ${bam2} --outdir ${TMPDIR} ${same} ${make_csv_flag}
 
-## Delete directories with zero results from bam_intersect.py
-num_chars=$(wc -c < "${outdir}/dsgrep_out.csv")
+## See if there is anything in dsgrep_out.csv, which is produced by bamintersect.py
+num_chars=$(wc -c < "${TMPDIR}/dsgrep_out.csv")
 echo "num_chars -c is:  ${num_chars}"
-if [ ${num_chars} -lt 1 ]; then
-    rm -rf "${outdir}"
-else
+
+## Let TMPDIR die if dsgrep_out.csv contains no output.
+if [ ${num_chars} -ge 1 ]; then
+    mv "${TMPDIR}" "${outdir}"
+
     ## Sort to prepare for consolidation later.
     sort-bed "${outdir}/dsgrep_out.csv" > "${outdir}/f1"
-    mv "${outdir}/f1"  "${outdir}/dsgrep_out.csv"
+    mv "${outdir}/f1" "${outdir}/dsgrep_out.csv"
 fi
 
