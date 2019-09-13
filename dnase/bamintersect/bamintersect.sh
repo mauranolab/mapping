5227e97cec8d37d20efcc5a99454d9de890e3a6e
cadley-nyulangone <45205158+cadley-nyulangone@users.noreply.github.com>
2019-09-13 13:51:09 -0400

    Replace sbatch with qsub, and some variable name changes (#71)
    * Replace sbatch with qsub, and some variable name changes
    
    fixes #70
    
    * Changed certain output directories and modified some variable names
    
    fixes #71

--- dnase/bamintersect/bamintersect.sh
+++ dnase/bamintersect/bamintersect.sh
@@ -24,32 +24,12 @@ echo ${outdir}      # "${INTERMEDIATEDIR}/bamintersectPyOut/${BASE1}___${BASE2}"
 ## reads_match=True means that we are looking to pair up read1's with read1's, and read2's with read2's.
 ##             True is also used for unpaired reads.
 ## reads_match=False means that we are looking to pair up read1's with read2's.
-
-## Set to True in the input file.
-if [ ${reads_match} = "False" ]; then
-    same=""
-else
-    same="--same"
-fi
-
-if [ ${make_csv} = "False" ]; then
-    make_csv_flag=""
-else
-    make_csv_flag="--make_csv"
-fi
-
-"${src}/bamintersect.py" --bam1 ${bam1} --bam2 ${bam2} --outdir ${TMPDIR} ${same} ${make_csv_flag}
+"${src}/bamintersect.py" --bam1 ${bam1} --bam2 ${bam2} --outdir ${TMPDIR} ${reads_match} ${make_csv}
 
 ## See if there is anything in dsgrep_out.csv, which is produced by bamintersect.py
-num_chars=$(wc -c < "${TMPDIR}/dsgrep_out.csv")
-echo "num_chars -c is:  ${num_chars}"
-
-## Let TMPDIR die if dsgrep_out.csv contains no output.
-if [ ${num_chars} -ge 1 ]; then
-    mv "${TMPDIR}" "${outdir}"
-
-    ## Sort to prepare for consolidation later.
-    sort-bed "${outdir}/dsgrep_out.csv" > "${outdir}/f1"
-    mv "${outdir}/f1" "${outdir}/dsgrep_out.csv"
+## If not, then let TMPDIR die. This reduces the number of merge operations needed in merge_bamintersect.sh
+if [ -s "${TMPDIR}/dsgrep_out.csv" ]; then
+    mkdir "${outdir}"
+    mv "${TMPDIR}/dsgrep_out.csv" ${outdir}
 fi
 
