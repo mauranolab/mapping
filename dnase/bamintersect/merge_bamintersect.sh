ea2f657a686d9a980db356af385d4026b0947a07
cadley-nyulangone <45205158+cadley-nyulangone@users.noreply.github.com>
2019-09-13 13:51:09 -0400

    Replace sbatch with qsub, and some variable name changes (#71)
    * Replace sbatch with qsub, and some variable name changes
    
    fixes #70
    
    * Changed certain output directories and modified some variable names
    
    fixes #71

--- dnase/bamintersect/merge_bamintersect.sh
+++ dnase/bamintersect/merge_bamintersect.sh
@@ -10,22 +10,22 @@ dir_names=($(ls -d ${INTERMEDIATEDIR}/bamintersectPyOut/*/))      ## These have 
 first="True"
 for i in ${dir_names[@]}; do
     if [ ${first} = "True" ]; then
-        cp "${i}dsgrep_out.csv" "${INTERMEDIATEDIR}/f1.bed"
+        cp "${i}dsgrep_out.csv" "${INTERMEDIATEDIR}/unsorted_dsgrep.bed"
         first="False"
     else
-        cat "${i}dsgrep_out.csv" >> "${INTERMEDIATEDIR}/f1.bed"
+        cat "${i}dsgrep_out.csv" >> "${INTERMEDIATEDIR}/unsorted_dsgrep.bed"
     fi
 done
 
 date
 echo Sorting...
-sort-bed "${INTERMEDIATEDIR}/f1.bed" > "${sampleOutdir}/${sample_name}.bed"
+sort-bed "${INTERMEDIATEDIR}/unsorted_dsgrep.bed" > "${sampleOutdir}/${sample_name}.bed"
 date
 
 ##########################################################################################################
 ## Create the final output tables.
 
-if [ ${make_csv} != "True" ]; then
+if [ ${make_csv} != "--make_csv" ]; then
     # Need the bed file to make the table.
     # Maybe make both bed and bam files, and delete bed file later?
     echo "Making 2 bam files, so exiting merge_bamintersect.sh prior to calling filter_tsv.bash"
@@ -41,28 +41,27 @@ fi
 echo -e "chromosome-bam2\tStart_Pos\tEnd_Pos\tWidth\tNearest_Gene\tPost-filter_Reads\tchromosome-bam1" > "${sampleOutdir}/${sample_name}.counts.txt"
 echo -e "chromosome-bam2\tStart_Pos\tEnd_Pos\tWidth\tNearest_Gene\tPost-filter_Reads\tchromosome-bam1" > "${sampleOutdir}/${sample_name}.informative.counts.txt"
 
+# Make sure this has not been left over from a previous run.
+rm -f "${sampleOutdir}/${sample_name}.informative.bed"
+
 # Merge the output related to each bam1 chromosome:
 while read main_chrom ; do
-    bedops --chrom ${main_chrom} --everything "${sampleOutdir}/${sample_name}.bed" > "${sampleOutdir}/${sample_name}.${main_chrom}.bed"
+    bedops --chrom ${main_chrom} --everything "${sampleOutdir}/${sample_name}.bed" > "${INTERMEDIATEDIR}/${sample_name}.${main_chrom}.bed"
 
-    ${src}/filter_tsv.sh ${sampleOutdir} ${bam1_5p_HA} ${bam1_3p_HA} ${sample_name} ${cegsgenome} ${annotationgenome} "NA" ${main_chrom} ${deletion_gene} ${deletion_range}
+    ${src}/filter_tsv.sh ${sampleOutdir} ${bam1_5p_HA} ${bam1_3p_HA} ${sample_name} ${cegsgenome} ${annotationgenome} "NA" ${main_chrom} ${deletion_gene} ${deletion_range} ${INTERMEDIATEDIR}
 
     ${src}/filter_tsv.sh ${sampleOutdir} ${bam1_5p_HA} ${bam1_3p_HA} ${sample_name} ${cegsgenome} ${annotationgenome} \
-                       ${exclude_regions_from_counts} ${main_chrom} ${deletion_gene} ${deletion_range}
+                       ${exclude_regions_from_counts} ${main_chrom} ${deletion_gene} ${deletion_range} ${INTERMEDIATEDIR}
 
-    cat "${sampleOutdir}/${sample_name}.${main_chrom}.informative.bed" >> "${sampleOutdir}/${sample_name}.informative.bed"
+    cat "${INTERMEDIATEDIR}/${sample_name}.${main_chrom}.informative.bed" >> "${sampleOutdir}/${sample_name}.informative.bed"
 
     ## Number of reads
-    n1=$(wc -l < "${sampleOutdir}/${sample_name}.${main_chrom}.bed")
+    n1=$(wc -l < "${INTERMEDIATEDIR}/${sample_name}.${main_chrom}.bed")
     ## Number of informative reads
-    n2=$(wc -l < "${sampleOutdir}/${sample_name}.${main_chrom}.informative.bed")
-
-    # Don't need to keep these in the output directory.
-    mv "${sampleOutdir}/${sample_name}.${main_chrom}.informative.bed" ${INTERMEDIATEDIR}
-    mv "${sampleOutdir}/${sample_name}.${main_chrom}.bed" ${INTERMEDIATEDIR}
+    n2=$(wc -l < "${INTERMEDIATEDIR}/${sample_name}.${main_chrom}.informative.bed")
 
     echo -e "Mapped reads with unmapped mates from ${main_chrom}:\t${n1}\tof which\t${n2}\tpassed through the filters and HAs, and are potentially informative.\n" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-done < "${sampleOutdir}/log/chrom_list1"
+done < "${sampleOutdir}/log/${sample_name}.chrom_list1"
 
 #############################################################################
 ## Cleanup
