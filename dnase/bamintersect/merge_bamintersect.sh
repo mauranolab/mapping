bc1573d250d018b0e079afb64ef3b9b7300e7da7
mattmaurano <maurano@nyu.edu>
2019-12-16 17:37:11 -0500

    Permit 1 mismatch Redo uninformative region handling to treat bam1/bam2 in parallel (chromosomes not present will be ignored) Add satellite repeats to uninformative regions merge_bamintersect.sh checked /genome1exclude.bed for whether to run HA_table.sh analysis. Can't figure out how that was ever right. reduce output to anc_info.txt so that log is linear simplify filter_tsv.sh by having command line argument determine output filename base move filtering filter_tsv.sh out of filter_tsv.sh into merge_bamintersect.sh

--- dnase/bamintersect/merge_bamintersect.sh
+++ dnase/bamintersect/merge_bamintersect.sh
@@ -29,7 +29,7 @@ if [ "${numElements}" = "0" ]; then
     # Don't generate an error. Create some appropriately empty output files.
     echo "" > "${sampleOutdir}/${sample_name}.bed"
     echo "" > "${sampleOutdir}/${sample_name}.informative.bed"
-    echo "There are no reads with unmapped mates to analyze." >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
+    echo "There are no reads with unmapped mates to analyze."
     # Not generating bam files for this case.
     
     ## Cleanup
@@ -49,7 +49,7 @@ for i in ${dir_names[@]}; do
     fi
 done
 
-sort-bed "${INTERMEDIATEDIR}/unsorted_dsgrep.bed" > "${sampleOutdir}/${sample_name}.bed"
+sort-bed ${INTERMEDIATEDIR}/unsorted_dsgrep.bed > ${sampleOutdir}/${sample_name}.bed
 
 debug_fa "Finished sorting dsgrep.bed"
 
@@ -64,47 +64,72 @@ if [ ${make_csv} != "--make_csv" ]; then
 fi
 
 if [ ${make_table} != "True" ]; then
-    echo "LP integration table not required."
+    echo "LP integration table not requested; quitting successfully."
     exit 0
 fi
 
-# First call to filter_tsv. Here we use all the reads.
-echo "Running filter_tsv without filters." >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-${src}/filter_tsv.sh ${sampleOutdir} ${sample_name} ${bam2genome} "${sampleOutdir}/${sample_name}.bed" all_reads_counts ${INTERMEDIATEDIR} null
+#Unfiltered
+${src}/filter_tsv.sh ${sampleOutdir}/${sample_name} ${sample_name} ${bam2genome} ${sampleOutdir}/${sample_name}.bed ${INTERMEDIATEDIR}
+echo
 
-# Second call to filter_tsv. Here we just use informative reads (not in the HAs, and not in the genome2exclude ranges.
-echo "Running filter_tsv with HA filters (if any) and with the Exclude Regions file (which may be empty)." >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-${src}/filter_tsv.sh ${sampleOutdir} ${sample_name} ${bam2genome} "${sampleOutdir}/${sample_name}.bed" informative_reads_counts ${INTERMEDIATEDIR} ${genome2exclude}
+
+#With filters
+if [ "${uninformativeRegionFile}" != "null" ];then
+    # Get reads that are in the deletion zone.
+    
+    #Starts out with bam1 coordinates
+    cat ${sampleOutdir}/${sample_name}.bed | 
+    #Remove bam1 reads that overlap the ranges defined in uninformativeRegionFile.bed file via submit_bamintrsect.sh
+    bedops -n 1 - ${uninformativeRegionFile} |
+    #Switch to bam2
+    awk -F "\t" 'BEGIN {OFS="\t"}; {print $7, $8, $9, $10, $11, $12, $1, $2, $3, $4, $5, $6}' | sort-bed - |
+    #Sort by the bam2 reads, then keep only the bam2 reads (and their bam1 mates) that do not overlap the uninformativeRegionFile:
+    bedops -n 1 - ${uninformativeRegionFile} |
+    #Only apply deletion_range to bam2
+    bedops -n 1 - ${INTERMEDIATEDIR}/deletion_range.bed | 
+    #Switch back to bam1 coordinates
+    awk -F "\t" 'BEGIN {OFS="\t"}; {print $7, $8, $9, $10, $11, $12, $1, $2, $3, $4, $5, $6}' | sort-bed - > ${sampleOutdir}/${sample_name}.informative.bed
+else
+    cp ${sampleOutdir}/${sample_name}.bed ${sampleOutdir}/${sample_name}.informative.bed
+fi
+${src}/filter_tsv.sh ${sampleOutdir}/${sample_name}.informative ${sample_name} ${bam2genome} ${sampleOutdir}/${sample_name}.informative.bed ${INTERMEDIATEDIR}
+echo
+
 
 # HA analysis:
-if [ -s "${INTERMEDIATEDIR}/genome1exclude.bed" ]; then
-    echo -e "Starting HA analysis.\n" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
+echo
+if [ -s "${INTERMEDIATEDIR}/HA_coords.bed" ]; then
+    echo -e "Starting HA analysis."
     ${src}/HA_table.sh ${bamname2} 3076 ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src}
-    ${src}/filter_tsv.sh ${sampleOutdir} "${sample_name}_HA" ${bam2genome} "${sampleOutdir}/${sample_name}_HA.bed" all_reads_counts ${INTERMEDIATEDIR} null
+    ${src}/filter_tsv.sh ${sampleOutdir}/${sample_name}_HA "${sample_name}_HA" ${bam2genome} ${sampleOutdir}/${sample_name}_HA.bed ${INTERMEDIATEDIR}
 else
-    echo -e "No HAs available, so there will be no HA analysis.\n" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
+    echo -e "No HAs available, so there will be no HA analysis."
     
     # Create empty files.
-    touch "${sampleOutdir}/${sample_name}_HA.counts.txt"
-    touch "${sampleOutdir}/${sample_name}_HA.bed"
-    touch "${sampleOutdir}/${sample_name}_HA.counts.anc_info.txt"
+    touch ${sampleOutdir}/${sample_name}_HA.counts.txt
+    touch ${sampleOutdir}/${sample_name}_HA.bed
 fi
+echo
 
-# Look for reads spanning the assembly and the backbone.
+
+echo
+echo "Look for reads spanning the assembly and the backbone."
 ${src}/assmblyBackbone_table.sh ${bamname1} 3076 ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src}
-${src}/filter_tsv.sh ${sampleOutdir} "${sample_name}_assmblyBackbone" ${bam2genome} "${sampleOutdir}/${sample_name}_assmblyBackbone.bed" all_reads_counts ${INTERMEDIATEDIR} null
+${src}/filter_tsv.sh ${sampleOutdir}/${sample_name}_assmblyBackbone "${sample_name}_assmblyBackbone" ${bam2genome} "${sampleOutdir}/${sample_name}_assmblyBackbone.bed" ${INTERMEDIATEDIR}
+echo
 
 # Number of reads
 n1=$(wc -l < "${sampleOutdir}/${sample_name}.bed")
 # Number of informative reads
 n2=$(wc -l < "${sampleOutdir}/${sample_name}.informative.bed")
 
-echo -e "Summary:" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-echo -e "Mapped reads with unmapped mates:\t${n1}\tof which\t${n2}\tpassed through the Exclude Regions filters and over the HAs (if any), and are potentially informative." >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-echo -e "May include backbone reads, and reads not on the integration site chromosome. Some or all may not be in the \"informative.counts\" table, if they are over 500 bps from the nearest other informative read.\n" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
+echo -e "Summary:" >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
+echo -e "Mapped reads with unmapped mates:\t${n1}\tof which\t${n2}\tpassed through the Exclude Regions filters and over the HAs (if any), and are potentially informative." >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
+echo -e "May include backbone reads, and reads not on the integration site chromosome. Some or all may not be in the \"informative.counts\" table, if they are over 500 bps from the nearest other informative read.\n" >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
 
 ###########################################################################################################################################
-# Subset bam files for uploading UCSC custom tracks:
+echo
+echo "Subset bam files for uploading UCSC custom tracks"
 cut -f4 "${sampleOutdir}/${sample_name}_assmblyBackbone.bed" > "${INTERMEDIATEDIR}/${sample_name}.readNames.txt"
 
 if [ -s "${INTERMEDIATEDIR}/${sample_name}.readNames.txt" ]; then
@@ -113,9 +138,7 @@ if [ -s "${INTERMEDIATEDIR}/${sample_name}.readNames.txt" ]; then
     samtools index ${sampleOutdir}/${sample_name}.assmblyBackbone.${bam1genome}.bam
 fi
 
-
 cut -f4 "${sampleOutdir}/${sample_name}.informative.bed" > "${INTERMEDIATEDIR}/${sample_name}.readNames.txt"
-
 if [ -s "${INTERMEDIATEDIR}/${sample_name}.readNames.txt" ]; then
     debug_fa "${sample_name}.readNames.txt was found"
     
@@ -129,9 +152,6 @@ if [ -s "${INTERMEDIATEDIR}/${sample_name}.readNames.txt" ]; then
     samtools index ${sampleOutdir}/${sample_name}.informative.${bam2genome}.bam
     debug_fa "Completed samtools lines for bam2"
     
-    echo "" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-    
-    echo "Paths for custom tracks to bam files with informative reads:" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
     
     #Prep for UCSC track links
     #Remove "new" from the end of path so that we can reprocess data without affecting live data
@@ -144,21 +164,16 @@ if [ -s "${INTERMEDIATEDIR}/${sample_name}.readNames.txt" ]; then
         UCSCbase="bigDataUrl=https://mauranolab@cascade.isg.med.nyu.edu/~mauram01/${projectdir}/${sampleOutdir}"
     fi
     
-    echo "track name=\"${sample_name}.${bam1genome}-reads\" description=\"${sample_name}.${bam1genome} Reads\" visibility=pack pairEndsByName=F maxWindowToDraw=10000 maxItems=250 type=bam ${UCSCbase}/${sample_name}.informative.${bam1genome}.bam" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
+    echo "" >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
+    echo "Paths for custom tracks to bam files with informative reads:" >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
+    echo "track name=\"${sample_name}.${bam1genome}-reads\" description=\"${sample_name}.${bam1genome} Reads\" visibility=pack pairEndsByName=F maxWindowToDraw=10000 maxItems=250 type=bam ${UCSCbase}/${sample_name}.informative.${bam1genome}.bam" >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
+    echo "track name=\"${sample_name}.${bam2genome}-reads\" description=\"${sample_name}.${bam2genome} Reads\" visibility=pack pairEndsByName=F maxWindowToDraw=10000 maxItems=250 type=bam ${UCSCbase}/${sample_name}.informative.${bam2genome}.bam" >> ${sampleOutdir}/${sample_name}.counts.anc_info.txt
     
-    echo "" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-    
-    echo "track name=\"${sample_name}.${bam2genome}-reads\" description=\"${sample_name}.${bam2genome} Reads\" visibility=pack pairEndsByName=F maxWindowToDraw=10000 maxItems=250 type=bam ${UCSCbase}/${sample_name}.informative.${bam2genome}.bam" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-    
-    echo "" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-    echo "" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
 else
-    debug_fa "${sample_name}.readNames.txt was not found"
-    
     # No informative reads
-    echo "For ${sample_name}, there are no bam files with informative reads." >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
-    echo "" >> "${sampleOutdir}/${sample_name}.counts.anc_info.txt"
+    echo "For ${sample_name}, there are no bam files with informative reads."
 fi
+echo
 
 debug_fa "Done with the samtools section."
 
