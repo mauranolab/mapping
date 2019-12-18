7a616f5c626455216d7f24809d8c1be727e4ee1d
mattmaurano <maurano@nyu.edu>
2019-12-18 09:55:32 -0500

    rationalize command line argument and variable names remove unneeded use of INTERMEDIATEDIR assemblyBackbone.bam doesn't need bam1genome in the name since it's implicit

--- dnase/bamintersect/bamintersect.sh
+++ dnase/bamintersect/bamintersect.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 ########################################################
 ## src is passed in via an sbatch export
 ## reads_match is passed in via an sbatch export
-## make_csv is passed in via an sbatch export
+## make_bed is passed in via an sbatch export
 ## INTERMEDIATEDIR is passed in via an sbatch export ($INTERMEDIATEDIR/sorted_bams contains output of sort_bamintersect.sh)
 ########################################################
 
@@ -16,7 +16,7 @@ read -r bam1 bam2 outdir <<< ${bam_intersect_data}
 ## reads_match=True means that we are looking to pair up read1's with read1's, and read2's with read2's.
 ##             True is also used for unpaired reads.
 ## reads_match=False means that we are looking to pair up read1's with read2's.
-${src}/bamintersect.py --src ${src} --bam1 ${bam1} --bam2 ${bam2} --outdir ${TMPDIR} --max_mismatches ${max_mismatches} ${reads_match} ${make_csv} ${ReqFullyAligned}
+${src}/bamintersect.py --src ${src} --bam1 ${bam1} --bam2 ${bam2} --outdir ${TMPDIR} --max_mismatches ${max_mismatches} ${reads_match} ${make_bed} ${ReqFullyAligned}
 
 ## See if there is anything in dsgrep_out.csv, which is produced by bamintersect.py
 ## If not, then let TMPDIR die. This reduces the number of merge operations needed in merge_bamintersect.sh
