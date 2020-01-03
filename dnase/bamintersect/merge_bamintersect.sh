mauranolab/mapping

commit 53f321fce063498698b7957cc69f8ca687a5d654

HA_table Changes (#140)
Handle samples with no chromosome backbone without error. fixes #137

Refactor exclude_flags to use 3084 for backbone analyses, and 3596 for HA analyses. fixes #118

Co-authored-by: mattmaurano <44711377+mattmaurano@users.noreply.github.com>

dnase/bamintersect/merge_bamintersect.sh
@@ -129,18 +129,20 @@ echo
 #Al
 if [ -s "${INTERMEDIATEDIR}/HA_coords.bed" ]; then
     echo -e "Starting HA analysis."
-    ${src}/HA_table.sh HA ${bam1} ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src} ${ReqFullyAligned}
-    ${src}/counts_table.sh ${sampleOutdir}/${sample_name}.HA "${sample_name}.HA" ${bam1genome} ${bam1genome} ${sampleOutdir}/${sample_name}.HA.bed
+
+    # Exclude reads with flags: read unmapped, mate unmapped, failed QC flag, read is duplicate, or read is sup alignment (3596).
+    ${src}/HA_table.sh HA ${bam2} ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src} 3596 ${ReqFullyAligned}
+    ${src}/counts_table.sh ${sampleOutdir}/${sample_name}.HA "${sample_name}.HA" ${bam2genome} ${sampleOutdir}/${sample_name}.HA.bed
 else
     echo -e "No HAs available, so there will be no HA analysis."
 fi
 echo
 
-
 echo
 echo "Look for reads spanning the assembly and the backbone."
-${src}/HA_table.sh AB ${bam2} ${sampleOutdir} ${sample_name} $INTERMEDIATEDIR{} ${src} ${ReqFullyAligned}
-${src}/counts_table.sh ${sampleOutdir}/${sample_name}.assemblyBackbone "${sample_name}.assemblyBackbone" ${bam2genome} ${bam2genome} ${sampleOutdir}/${sample_name}.assemblyBackbone.bed
+# Exclude reads with flags: read unmapped, mate unmapped, read is duplicate, or read is sup alignment (3084).
+${src}/HA_table.sh AB ${bam1} ${sampleOutdir} ${sample_name} $INTERMEDIATEDIR{} ${src} 3084 ${ReqFullyAligned}
+${src}/counts_table.sh ${sampleOutdir}/${sample_name}.assemblyBackbone "${sample_name}.assemblyBackbone" ${bam2genome} ${sampleOutdir}/${sample_name}.assemblyBackbone.bed
 echo
 
 # Number of reads

dnase/bamintersect/HA_table.sh
@@ -15,40 +15,43 @@ sampleOutdir=$3
 sample_name=$4
 INTERMEDIATEDIR=$5
 src=$6
-ReqFullyAligned=$7
-
-# Exclude reads with flags: read unmapped, mate unmapped, failed QC flag, read is duplicate, or read is sup alignment.
-exclude_flags=3596
+exclude_flags=$7
+ReqFullyAligned=$8
 
 ######################################################################
 
 if [ ${runHA} = "AB" ]; then
     echo "Generating assemblyBackbone table"
-
+    
     # Assume there are only 2 chromosomes: the assembly and the assembly backbone, with the assembly first.
     read -r assembly assembly_readlength all_other <<< $(samtools idxstats ${bamfile})
-
-    # Make sure the naming conventions are being followed.
-    backbone=${assembly}_backbone
-    backboneCheck=$(samtools idxstats ${bamfile} | tail -n +2 | head -n 1 | cut -f1)
-    if [ "${backbone}" != "${backboneCheck}" ]; then
-        echo "[assemblyBackbone_table.sh] ERROR: Problem with assignment of backbone names: ${assembly} ${backbone} ${backboneCheck}"
+    
+    # Make sure the naming conventions are being followed, since some manual effort goes into making these assembly files.
+    # We require only two chromosomes: an [assembly] and an [assembly]_backbone
+    # If there is a misspelling, extra chromosomes, or if they are out of order, an error happens.
+    backbone=$(samtools idxstats ${bamfile} | tail -n +2 | head -n 1 | cut -f1)  # This should be the backbone chromosome.
+    numChroms=$(samtools idxstats ${bamfile} | awk -F "\t" 'BEGIN {OFS="\t"} $1!="*"' | wc -l)
+    if [[ "${numChroms}" != 2 ]]; then
+        echo "${assembly} has wrong number of chromosomes, quitting successfully."
+        touch "${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
+        exit 0
+    elif [ "${backbone}" != "${assembly}_backbone" ]; then
+        # The second chromosome is not named [assembly]_backbone
+        echo "WARNING: Assembly has two chromosomes but the second does not match the expected backbone name, quitting successfully: ${assembly} ${backbone}"
         echo ""
         echo "samtools idxstats ${bamfile} :"
         samtools idxstats ${bamfile}
-        # exit 1
-    
-        # For now:
         touch "${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
         exit 0
+    else
+        # The naming conventions are being followed.
+        echo assembly is: ${assembly}
+        echo backbone is: ${backbone}
+        
+        # Define Zone 1. We want it to be the entire assembly.
+        echo -e "${assembly}\t0\t${assembly_readlength}" > ${TMPDIR}/zone1.bed
+        outputBed="${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
     fi
-    echo assembly is:  ${assembly}
-    echo backbone is: ${backbone}
-
-    # Define Zone 1. We want it to be the entire assembly.
-    echo -e "${assembly}\t0\t${assembly_readlength}" > ${TMPDIR}/zone1.bed
-
-    outputBed="${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
 elif [ ${runHA} = "HA" ]; then
     echo "Generating HA table"
     # Define Zone 1. We want it to be the HAs (both of them).
@@ -101,4 +104,3 @@ ${src}/bamintersect.py --src ${src} --bam1 "${TMPDIR}/bamfile_Zone1reads.bam" --
 
 sort-bed ${sampleOutdir}/dsgrep_out.csv > ${outputBed}
 rm -f ${sampleOutdir}/dsgrep_out.csv
-

commit ff89d9016df27f31bdcb469d8750c48f7be96c51

fix merge regression from 53f321fce063498698b7957cc69f8ca687a5d654 and PR #140

dnase/bamintersect/merge_bamintersect.sh
@@ -132,17 +132,18 @@ if [ -s "${INTERMEDIATEDIR}/HA_coords.bed" ]; then
 
     # Exclude reads with flags: read unmapped, mate unmapped, failed QC flag, read is duplicate, or read is sup alignment (3596).
     ${src}/HA_table.sh HA ${bam2} ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src} 3596 ${ReqFullyAligned}
-    ${src}/counts_table.sh ${sampleOutdir}/${sample_name}.HA "${sample_name}.HA" ${bam2genome} ${sampleOutdir}/${sample_name}.HA.bed
+    ${src}/counts_table.sh ${sampleOutdir}/${sample_name}.HA "${sample_name}.HA" ${bam1genome} ${bam1genome} ${sampleOutdir}/${sample_name}.HA.bed
 else
     echo -e "No HAs available, so there will be no HA analysis."
 fi
 echo
 
+
 echo
 echo "Look for reads spanning the assembly and the backbone."
 # Exclude reads with flags: read unmapped, mate unmapped, read is duplicate, or read is sup alignment (3084).
 ${src}/HA_table.sh AB ${bam1} ${sampleOutdir} ${sample_name} $INTERMEDIATEDIR{} ${src} 3084 ${ReqFullyAligned}
-${src}/counts_table.sh ${sampleOutdir}/${sample_name}.assemblyBackbone "${sample_name}.assemblyBackbone" ${bam2genome} ${sampleOutdir}/${sample_name}.assemblyBackbone.bed
+${src}/counts_table.sh ${sampleOutdir}/${sample_name}.assemblyBackbone "${sample_name}.assemblyBackbone" ${bam2genome} ${bam2genome} ${sampleOutdir}/${sample_name}.assemblyBackbone.bed
 echo
 
 # Number of reads