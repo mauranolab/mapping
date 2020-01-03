2f28347569f6857d37fc0729dca8f7b71963bdc2
mattmaurano <maurano@nyu.edu>
2020-01-03 06:52:04 -0500

    fix merge regression from 53f321fce063498698b7957cc69f8ca687a5d654 and PR #140

--- dnase/bamintersect/merge_bamintersect.sh
+++ dnase/bamintersect/merge_bamintersect.sh
@@ -131,7 +131,7 @@ if [ -s "${INTERMEDIATEDIR}/HA_coords.bed" ]; then
     echo -e "Starting HA analysis."
 
     # Exclude reads with flags: read unmapped, mate unmapped, failed QC flag, read is duplicate, or read is sup alignment (3596).
-    ${src}/HA_table.sh HA ${bam2} ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src} 3596 ${ReqFullyAligned}
+    ${src}/HA_table.sh HA ${bam1} ${sampleOutdir} ${sample_name} ${INTERMEDIATEDIR} ${src} 3596 ${ReqFullyAligned}
     ${src}/counts_table.sh ${sampleOutdir}/${sample_name}.HA "${sample_name}.HA" ${bam1genome} ${bam1genome} ${sampleOutdir}/${sample_name}.HA.bed
 else
     echo -e "No HAs available, so there will be no HA analysis."
@@ -142,7 +142,7 @@ echo
 echo
 echo "Look for reads spanning the assembly and the backbone."
 # Exclude reads with flags: read unmapped, mate unmapped, read is duplicate, or read is sup alignment (3084).
-${src}/HA_table.sh AB ${bam1} ${sampleOutdir} ${sample_name} $INTERMEDIATEDIR{} ${src} 3084 ${ReqFullyAligned}
+${src}/HA_table.sh AB ${bam2} ${sampleOutdir} ${sample_name} $INTERMEDIATEDIR{} ${src} 3084 ${ReqFullyAligned}
 ${src}/counts_table.sh ${sampleOutdir}/${sample_name}.assemblyBackbone "${sample_name}.assemblyBackbone" ${bam2genome} ${bam2genome} ${sampleOutdir}/${sample_name}.assemblyBackbone.bed
 echo
 
