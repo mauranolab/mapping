2ba9d492781587af401b3bef6ee8d79cf84f9f31
mattmaurano <44711377+mattmaurano@users.noreply.github.com>
2020-01-03 11:45:40 +0100

    simplify

--- dnase/bamintersect/HA_table.sh
+++ dnase/bamintersect/HA_table.sh
@@ -22,30 +22,22 @@ ReqFullyAligned=$8
 
 if [ ${runHA} = "AB" ]; then
     echo "Generating assemblyBackbone table"
-
+    
     # Assume there are only 2 chromosomes: the assembly and the assembly backbone, with the assembly first.
     read -r assembly assembly_readlength all_other <<< $(samtools idxstats ${bamfile})
-
+    
     # Make sure the naming conventions are being followed, since some manual effort goes into making these assembly files.
     # We require only two chromosomes: an [assembly] and an [assembly]_backbone
     # If there is a misspelling, extra chromosomes, or if they are out of order, an error happens.
-    backbone=${assembly}_backbone
-    backboneCheck=$(samtools idxstats ${bamfile} | tail -n +2 | head -n 1 | cut -f1)  # This should be the backbone chromosome.
+    backbone=$(samtools idxstats ${bamfile} | tail -n +2 | head -n 1 | cut -f1)  # This should be the backbone chromosome.
     numChroms=$(samtools idxstats ${bamfile} | awk -F "\t" 'BEGIN {OFS="\t"} $1!="*"' | wc -l)
-
-    if [[ "${numChroms}" > 2 ]]; then
-        # There are three or more chromosomes.
-        echo "${assembly} has too many chromosomes."
+    if [[ "${numChroms}" != 2 ]]; then
+        echo "${assembly} has wrong number of chromosomes, quitting successfully."
         touch "${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
         exit 0
-    elif [[ "${numChroms}" == "1" ]]; then
-        # There is no backbone, only a single [assembly]. As an example, this happens for pSpCas9.
-        echo "${assembly} does not have a backbone chromosome."
-        touch "${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
-        exit 0
-    elif [ "${backbone}" != "${backboneCheck}" ]; then
-        # We are not getting [assembly] and [assembly]_backbone
-        echo "[assemblyBackbone_table.sh] ERROR: Problem with assignment of backbone names: ${assembly} ${backbone} ${backboneCheck}"
+    elif [ "${backbone}" != "${assembly}_backbone" ]; then
+        # The second chromosome is not named [assembly]_backbone
+        echo "WARNING: Assembly has two chromosomes but the second does not match the expected backbone name, quitting successfully: ${assembly} ${backbone}"
         echo ""
         echo "samtools idxstats ${bamfile} :"
         samtools idxstats ${bamfile}
@@ -55,7 +47,7 @@ if [ ${runHA} = "AB" ]; then
         # The naming conventions are being followed.
         echo assembly is: ${assembly}
         echo backbone is: ${backbone}
-
+        
         # Define Zone 1. We want it to be the entire assembly.
         echo -e "${assembly}\t0\t${assembly_readlength}" > ${TMPDIR}/zone1.bed
         outputBed="${sampleOutdir}/${sample_name}.assemblyBackbone.bed"
@@ -112,4 +104,3 @@ ${src}/bamintersect.py --src ${src} --bam1 "${TMPDIR}/bamfile_Zone1reads.bam" --
 
 sort-bed ${sampleOutdir}/dsgrep_out.csv > ${outputBed}
 rm -f ${sampleOutdir}/dsgrep_out.csv
-
