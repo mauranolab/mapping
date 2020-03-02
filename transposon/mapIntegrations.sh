81ccbde170fd3e49b4af666bff726ad232425195
André M. Ribeiro-dos-Santos <andremrsantos@gmail.com>
2020-03-02 15:44:41 -0500

    fix pre-filter sort to use previous parameters

--- transposon/mapIntegrations.sh
+++ transposon/mapIntegrations.sh
@@ -107,7 +107,7 @@ else
     pigz -p ${NSLOTS} -c -1 > $TMPDIR/${sample}.R1.fastq.gz
 
     bwaAlnOpts="-t ${NSLOTS} -Y -r @RG\\tID:${sample}\\tLB:$DS\\tSM:${DS_nosuffix}"
-    extractcmd="mem ${bwaAlnOpts} ${bwaIndex} $TMPDIR/${sample}.R1.fastq.gz $TMPDIR/${sample}.R2.fastq.gz | samtools sort -n -@ $NSLOTS -m 1750M -"
+    extractcmd="mem ${bwaAlnOpts} ${bwaIndex} $TMPDIR/${sample}.R1.fastq.gz $TMPDIR/${sample}.R2.fastq.gz | samtools sort -@ $NSLOTS -O bam -T $OUTDIR/${sample}.sortbyname -l 1 -n -"
 fi
 
 echo "Extracting"
