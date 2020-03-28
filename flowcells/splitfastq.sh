a65e148888dda90afe6e7cd7ca08db763a6ec3a8
mattmaurano <maurano@nyu.edu>
2020-03-27 21:23:00 -0400

    * support splitting SE runs * switch splitfastq.sh to process one file at a time * handle Project_SARS

--- flowcells/splitfastq.sh
+++ flowcells/splitfastq.sh
@@ -3,15 +3,14 @@ set -eu -o pipefail
 
 #Based on stampipes/scripts/fastq/splitfastq.bash
 #Requires sequences/qualities to be on single line
-#NB doesn't work with SE fastq files
 
 
 module add pigz
 
 
 SAMPLE_NAME=$1
-R1_FASTQ=$2
-R2_FASTQ=$3
+FASTQ=$2
+SUFFIX=$3
 CHUNK_SIZE=$4
 
 if [ "$#" -eq 5 ]; then
@@ -28,27 +27,15 @@ mkdir -p ${TMPDIR}/splitfastq.${SAMPLE_NAME}
 echo "Splitting FASTQ files into ${CHUNK_SIZE} reads using ${TMPDIR}/splitfastq.${SAMPLE_NAME}"
 date
 
-echo "Splitting R1 ${R1_FASTQ}"
-pigz -dc ${R1_FASTQ} | split -l ${LINE_CHUNK} -d -a 3 - "${TMPDIR}/splitfastq.${SAMPLE_NAME}/${SAMPLE_NAME}_R1_"
+echo "Splitting ${SUFFIX} ${FASTQ}"
+pigz -dc ${FASTQ} | split -l ${LINE_CHUNK} -d -a 3 - "${TMPDIR}/splitfastq.${SAMPLE_NAME}/${SAMPLE_NAME}_${SUFFIX}_"
 date
 
-#BUGBUG doesn't work with SE fastq file
-if [ true ]; then
-    echo "Splitting R2 ${R2_FASTQ}"
-    pigz -dc ${R2_FASTQ} | split -l ${LINE_CHUNK} -d -a 3 - "${TMPDIR}/splitfastq.${SAMPLE_NAME}/${SAMPLE_NAME}_R2_"
-    date
-fi
 
-SPLIT_COUNT=`ls ${TMPDIR}/splitfastq.${SAMPLE_NAME}/${SAMPLE_NAME}_R1_??? | wc -l`
+SPLIT_COUNT=`ls ${TMPDIR}/splitfastq.${SAMPLE_NAME}/${SAMPLE_NAME}_${SUFFIX}_??? | wc -l`
 
-orig_dir=`dirname ${R1_FASTQ}`
-orig_dir2=`dirname ${R2_FASTQ}`
-if [[ "${orig_dir}" != "${orig_dir2}" ]]; then
-    echo "ERROR: ${R1_FASTQ} in different location than ${R2_FASTQ}"
-    exit 2
-fi
-
-for RAW_FILE in `find ${TMPDIR}/splitfastq.${SAMPLE_NAME} -name "${SAMPLE_NAME}_R?_???"`; do
+orig_dir=`dirname ${FASTQ}`
+for RAW_FILE in `find ${TMPDIR}/splitfastq.${SAMPLE_NAME} -name "${SAMPLE_NAME}_${SUFFIX}_${SUFFIX}_???"`; do
     FASTQ_FILENAME=`basename ${RAW_FILE}`
     if [ -e "${orig_dir}/${FASTQ_FILENAME}.fastq.gz" ]; then
         echo "ERROR: ${orig_dir}/${FASTQ_FILENAME}.fastq.gz already exists!"
@@ -64,13 +51,12 @@ done
 
 
 if [ -z "${FASTQ_BAK}" ]; then
-    echo "Removing original files"
-    rm -f ${R1_FASTQ} ${R2_FASTQ}
+    echo "Removing original file"
+    rm -f ${FASTQ}
 else
-    echo "Backing up files to ${FASTQ_BAK}"
+    echo "Backing up file to ${FASTQ_BAK}"
     mkdir -p ${FASTQ_BAK}
-    mv ${R1_FASTQ} ${FASTQ_BAK}
-    mv ${R2_FASTQ} ${FASTQ_BAK}
+    mv ${FASTQ} ${FASTQ_BAK}
 fi
 
 
