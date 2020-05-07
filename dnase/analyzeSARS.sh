7094d1c6bf03499680fa08effaef9b94ec89f0f9
mattmaurano <maurano@nyu.edu>
2020-05-07 16:49:34 -0400

    cleanup

--- dnase/analyzeSARS.sh
+++ dnase/analyzeSARS.sh
@@ -19,7 +19,6 @@ for bamfile in `find ${dir} -mindepth 2 -name "*.bam" -not -name "*R1R2*" -not -
     sample=`basename ${bamfile} .hg38_full_wuhCor1.bam`
     sampleid=`echo ${sample} | cut -d "-" -f1`
     bs=`echo ${sample} | cut -d "-" -f2`
-    base=`basename ${bamfile} .hg38_full_wuhCor1.bam`
     echo -n -e "${fc}\t${sampletype}\t${sampleid}\t${bs}\t"
     
     samtools view -F 1024 -c ${bamfile} NC_045512v2 | perl -pe 's/\n/\t/g;'
@@ -29,8 +28,7 @@ for bamfile in `find ${dir} -mindepth 2 -name "*.bam" -not -name "*R1R2*" -not -
     coveragefile=`echo ${bamfile} | perl -pe 's/.bam/.coverage.starch/g;'`
     awk -F "\t" '{OFS="\t"; print $1, 0, $2}' /vol/isg/annotation/fasta/wuhCor1/wuhCor1.chrom.sizes | bedmap --prec 0 --wmean - ${coveragefile} | perl -pe 's/NAN$/0/g;' | perl -pe 's/\n/\t/g;'
     
-    covfile=`echo ${bamfile} | perl -pe 's/\.bam$/.coverage.starch/g;'`
-    unstarch NC_045512v2 ${covfile} | awk '$5>=20' | awk -F "\t" 'BEGIN {sum=0} {sum+=$3-$2} END {print sum}'
+    unstarch NC_045512v2 ${coveragefile} | awk '$5>=20' | awk -F "\t" 'BEGIN {sum=0} {sum+=$3-$2} END {print sum}'
 done >> ${dir}/viralcoverage.txt
 
 
