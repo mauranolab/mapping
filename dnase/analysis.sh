fb04bbb29710043e695cd4e7c36b83381cbbe2ab
mattmaurano <maurano@nyu.edu>
2020-06-01 22:26:55 -0400

    relax Xmx to avoid crashes due to resource constraints

--- dnase/analysis.sh
+++ dnase/analysis.sh
@@ -328,7 +328,7 @@ if [[ "${sampleType}" == "dna" ]] || [[ "${sampleType}" == "capture" ]] || [[ "$
         echo "Collecting picard metrics"
         mkdir -p ${sampleOutdir}/picardmetrics
         #TODO should we exclude flag 512 like for CollectWgsMetrics?
-        java -XX:ParallelGCThreads=1 -Xmx2g -Dpicard.useLegacyParser=false -jar ${PICARDPATH}/picard.jar CollectMultipleMetrics -INPUT ${sampleOutdir}/${name}.${mappedgenome}.bam -REFERENCE_SEQUENCE ${referencefasta} -OUTPUT ${sampleOutdir}/picardmetrics/${name}.${mappedgenome} -PROGRAM CollectGcBiasMetrics -VERBOSITY WARNING
+        java -XX:ParallelGCThreads=1 -Xmx5g -Dpicard.useLegacyParser=false -jar ${PICARDPATH}/picard.jar CollectMultipleMetrics -INPUT ${sampleOutdir}/${name}.${mappedgenome}.bam -REFERENCE_SEQUENCE ${referencefasta} -OUTPUT ${sampleOutdir}/picardmetrics/${name}.${mappedgenome} -PROGRAM CollectGcBiasMetrics -VERBOSITY WARNING
         #TODO could use CollectHsMetrics but needs bait coordinates
         
         echo
@@ -336,7 +336,7 @@ if [[ "${sampleType}" == "dna" ]] || [[ "${sampleType}" == "capture" ]] || [[ "$
         #Need to make second pass as CollectWgsMetrics can't be run by CollectMultipleMetrics
         #Exclude flag 512 rather than it's default MAPQ/BQ filters
         #NB I often see this using 4-5% of memory, about twice the available per-job average
-        samtools view -h -u ${samflags} ${sampleOutdir}/${name}.${mappedgenome}.bam | java -XX:ParallelGCThreads=1 -Xmx2g -Dpicard.useLegacyParser=false -jar ${PICARDPATH}/picard.jar CollectWgsMetrics -INPUT /dev/stdin -REFERENCE_SEQUENCE ${referencefasta} -OUTPUT ${sampleOutdir}/picardmetrics/${name}.${mappedgenome}.wgsmetrics -VERBOSITY WARNING -COUNT_UNPAIRED=true
+        samtools view -h -u ${samflags} ${sampleOutdir}/${name}.${mappedgenome}.bam | java -XX:ParallelGCThreads=1 -Xmx5g -Dpicard.useLegacyParser=false -jar ${PICARDPATH}/picard.jar CollectWgsMetrics -INPUT /dev/stdin -REFERENCE_SEQUENCE ${referencefasta} -OUTPUT ${sampleOutdir}/picardmetrics/${name}.${mappedgenome}.wgsmetrics -VERBOSITY WARNING -COUNT_UNPAIRED=true
         
         #NB included reads don't exactly match our -F 512 or duplicates
         #but aren't generating a binned coverage track
