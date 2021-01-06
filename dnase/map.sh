mauranolab/mapping

commit 310ca5195daa75c95d54329a47d1a5b2318f8c61

add support for new amplicon SampleType, which uses Swift tool to soft-clip primers from aligned BAM file and disables duplicate marking add untested minimap2 support for ONT

dnase/map.sh
@@ -315,7 +315,6 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
         
         
         #Previously used -n 10 but never really used XA tag and maybe was causing sampe to occasionally truncate last line of output (dropping the tags)
-        #TODO perhaps I should add -N to get complete set of hits?
         bwaAlnExtractOpts="-n 3 -r ${readgroup}"
         if [[ "$PErun" == "TRUE" ]] ; then
             echo -e "\nMapping R2 ${reads2fq} for ${sample2}"
@@ -325,7 +324,7 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
             bwa aln ${bwaAlnOpts} ${bwaIndex} $TMPDIR/${sample2}.fastq.gz > $TMPDIR/${sample2}.${curGenome}.sai
             
             #-P didn't have a major effect, but some jobs were ~10-40% faster but takes ~16GB RAM instead of 4GB
-            extractcmd="sampe ${bwaAlnExtractOpts} -a ${maxInsertSize} ${bwaIndex} $TMPDIR/${sample1}.${curGenome}.sai $TMPDIR/${sample2}.${curGenome}.sai $TMPDIR/${sample1}.fastq.gz $TMPDIR/${sample2}.fastq.gz"
+            extractcmd="bwa sampe ${bwaAlnExtractOpts} -a ${maxInsertSize} ${bwaIndex} $TMPDIR/${sample1}.${curGenome}.sai $TMPDIR/${sample2}.${curGenome}.sai $TMPDIR/${sample1}.fastq.gz $TMPDIR/${sample2}.fastq.gz"
             
             #Only map unpaired reads if the file nonzero
             if [[ "${unpairedReadLines}" > 0 ]]; then
@@ -339,7 +338,7 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
                 echo
                 echo "Extracting unpaired reads"
                 unpairedReadsSam="$TMPDIR/${curfile}.${curGenome}.unpaired.sam"
-                unpairedExtractcmd="samse ${bwaAlnExtractOpts} ${bwaIndex} $TMPDIR/${curfile}.unpaired.${curGenome}.sai $TMPDIR/${curfile}.unpaired.fastq.gz"
+                unpairedExtractcmd="bwa samse ${bwaAlnExtractOpts} ${bwaIndex} $TMPDIR/${curfile}.unpaired.${curGenome}.sai $TMPDIR/${curfile}.unpaired.fastq.gz"
                 echo -e "unpairedExtractcmd=bwa $unpairedExtractcmd | (...)"
                 bwa $unpairedExtractcmd | grep -v "^@" > ${unpairedReadsSam}
                 
@@ -347,18 +346,23 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
             fi
             #TODO merge headers instead of dropping
         else
-            extractcmd="samse ${bwaAlnExtractOpts} ${bwaIndex} $TMPDIR/${sample1}.${curGenome}.sai $TMPDIR/${sample1}.fastq.gz"
+            extractcmd="bwa samse ${bwaAlnExtractOpts} ${bwaIndex} $TMPDIR/${sample1}.${curGenome}.sai $TMPDIR/${sample1}.fastq.gz"
         fi
     elif [[ "${processingCommand}" == "mapBwaMem" ]]; then
         #parameters from ira hall consensus pipeline added 2020feb18 along with update to bwa 0.7.17        #https://github.com/CCDG/Pipeline-Standardization/blob/master/PipelineStandard.md
         #I could not find any effect of '-Y'; in some spot checking I hever see bwa output anything with $6~/H/, though I can find some soft-clipped supplementary alignments with or without the option
         bwaMemOptions="-Y -K 100000000"
         
-        extractcmd="mem ${bwaMemOptions} ${userAlnOptions} -t ${NSLOTS} -R ${readgroup} ${bwaIndex} $TMPDIR/${sample1}.fastq.gz"
+        extractcmd="bwa mem ${bwaMemOptions} ${userAlnOptions} -t ${NSLOTS} -R ${readgroup} ${bwaIndex} $TMPDIR/${sample1}.fastq.gz"
         if [[ "$PErun" == "TRUE" ]] ; then
             extractcmd="${extractcmd} $TMPDIR/${sample2}.fastq.gz"
         fi
         echo "bwa ${extractcmd}"
+    elif [[ "${processingCommand}" == "mapMinimap" ]]; then
+        #Assumes SE
+        #note this ignores the trimmed reads in $TMPDIR/${sample1}.fastq.gz
+        extractcmd="minimap2 -a -x map-ont ${referencefasta} ${readsFq}"
+        echo "${extractcmd} ..."
     else
         echo "ERROR: impossible unsupported ${processingCommand}"
         exit 6
@@ -367,10 +371,10 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
     date
     echo
     echo "Extracting"
-    echo -e "extractcmd=bwa ${extractcmd} | (...)"
+    echo -e "extractcmd=${extractcmd} | (...)"
     date
     echo
-    bwa ${extractcmd} > $TMPDIR/${curfile}.${curGenome}.bwaout.bam
+    ${extractcmd} > $TMPDIR/${curfile}.${curGenome}.bwaout.bam
     
     
     echo
@@ -411,6 +415,23 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
     
     echo
     date
+    
+    
+    if [[ "${sampleType}" == "amplicon" ]]; then
+        echo
+        echo "Primer soft clipping"
+        samtools view -h ${sampleOutdir}/${curfile}.${curGenome}.bam |
+        #TODO hardcoded primer coords for now
+        #BUGBUG primerclip leaves no PG record; otherwise would set samtools view --no-PG
+        #BUGBUG dumps masterparsefails.log into directory
+        #BUGBUG can't stream to stdout; passes log output through stdout?
+        primerclip /vol/sars/sequences/wuhCor1/Swift_Amplicons/sarscov2_masterfile.txt /dev/stdin  $TMPDIR/${curfile}.${curGenome}.sam
+        samtools view -@ $NSLOTS -O bam -1 $TMPDIR/${curfile}.${curGenome}.sam > ${sampleOutdir}/${curfile}.${curGenome}.bam
+        
+        echo
+        date
+    fi
+    
     
     #Add MC tag containing mate CIGAR for duplicate calling
     #Why do I need this? samblaster can add this itself but seems to miss some?

dnase/analysis.sh
@@ -20,7 +20,7 @@ src=$6
 sampleType=`echo "${analysisType}" | awk -F "," '{print $2}'`
 
 
-if [[ "${sampleType}" != "none" ]] && [[ "${sampleType}" != "atac" ]] && [[ "${sampleType}" != "dnase" ]] && [[ "${sampleType}" != "chipseq" ]] && [[ "${sampleType}" != "dna" ]] && [[ "${sampleType}" != "capture" ]]; then
+if [[ "${sampleType}" != "none" ]] && [[ "${sampleType}" != "atac" ]] && [[ "${sampleType}" != "dnase" ]] && [[ "${sampleType}" != "chipseq" ]] && [[ "${sampleType}" != "dna" ]] && [[ "${sampleType}" != "capture" ]] && [[ "${sampleType}" != "amplicon" ]]; then
     echo "ERROR: unknown analysis command ${sampleType} in analysisType ${analysisType}"
     exit 1
 fi
@@ -143,6 +143,8 @@ case "${sampleType}" in
         ucscTrackDescriptionDataType="DNA";;
     capture)
         ucscTrackDescriptionDataType="Capture (${sampleAnnotationBait})";;
+    amplicon)
+        ucscTrackDescriptionDataType="Amplicon";;
     dnase)
         ucscTrackDescriptionDataType="DNase-seq";;
     atac)
@@ -280,7 +282,7 @@ echo "Making BAM track"
 echo "track name=${ucscName}-reads description=\"${ucscTrackDescriptionDataType} Reads ${name} (${analyzedReadsM}M reads analyzed)\" visibility=pack pairEndsByName=on maxItems=50000 type=bam ${UCSCbase}/${name}.${mappedgenome}.bam"
 
 
-if [[ "${sampleType}" == "dna" ]] || [[ "${sampleType}" == "capture" ]]; then
+if [[ "${sampleType}" == "dna" ]] || [[ "${sampleType}" == "capture" ]] || [[ "${sampleType}" == "amplicon" ]]; then
     if [ "${PFalignments}" -lt 50000000 ] && [[ "${analyzedReads}" > 0 ]]; then
         #Call hotspot 2 for debugging weird issues in coverage tracks, but not on large datasets
         callHotspots1=0
@@ -647,7 +649,7 @@ echo -e "Num_SE_pass_filter_alignments\t${PFalignmentsSE}\t\t${name}\t${mappedge
 printfNA "Num_SE_analyzed_reads\t${analyzedReadsSE}\t%.1f%%\t${name}\t${mappedgenome}\n" "${pctanalyzedReadsSE}"
 
 
-if [[ "${sampleType}" == "dna" ]] || [[ "${sampleType}" == "capture" ]]; then
+if [[ "${sampleType}" == "dna" ]] || [[ "${sampleType}" == "capture" ]] || [[ "${sampleType}" == "amplicon" ]]; then
     #Repeat it here for ease of parsing
     echo -e "Genomic_coverage\t\t${genomecov}\t${name}\t${mappedgenome}"
 fi

dnase/merge.sh
@@ -14,7 +14,7 @@ mappedgenome=$4
 src=$5
 
 processingCommand=`echo "${analysisType}" | awk -F "," '{print $1}'`
-#analysisCommand=`echo "${analysisType}" | awk -F "," '{print $2}'`
+sampleType=`echo "${analysisType}" | awk -F "," '{print $2}'`
 
 name=`basename ${sampleOutdir}`
 
@@ -110,7 +110,11 @@ else
     samtools merge -c -@ $NSLOTS ${mergeOpts} ${sampleOutdir}/${name}.${mappedgenome}.bam ${files}
 fi
 
-if [[ "${processingCommand}" =~ ^map ]] || [[ "${processingCommand}" == "aggregateRemarkDups" ]]; then
+if [[ "${sampleType}" == "amplicon" ]]; then
+    #No duplicate marking, just sort by coordinate
+    samtools sort -@ $NSLOTS -O bam -m 5000M -T $TMPDIR/${name}.sort -l 9 ${sampleOutdir}/${name}.${mappedgenome}.bam > ${sampleOutdir}/${name}.${mappedgenome}.new.bam
+    mv ${sampleOutdir}/${name}.${mappedgenome}.new.bam ${sampleOutdir}/${name}.${mappedgenome}.bam
+elif [[ "${processingCommand}" =~ ^map ]] || [[ "${processingCommand}" == "aggregateRemarkDups" ]]; then
     echo
     echo "Marking duplicates"
     date
@@ -136,7 +140,8 @@ if [[ "${processingCommand}" =~ ^map ]] || [[ "${processingCommand}" == "aggrega
     #https://github.com/GregoryFaust/samblaster/blob/master/samblaster.cpp
     ${src}/fixDupSAMHeaderPG.py - - >$TMPDIR/${name}.${mappedgenome}.markedDups.bam
     
-    samtools sort -@ $NSLOTS -O bam -m 5000M -T $TMPDIR/${name}.sort -l 9 $TMPDIR/${name}.${mappedgenome}.markedDups.bam > ${sampleOutdir}/${name}.${mappedgenome}.markedDups.bam && mv ${sampleOutdir}/${name}.${mappedgenome}.markedDups.bam ${sampleOutdir}/${name}.${mappedgenome}.bam
+    samtools sort -@ $NSLOTS -O bam -m 5000M -T $TMPDIR/${name}.sort -l 9 $TMPDIR/${name}.${mappedgenome}.markedDups.bam > ${sampleOutdir}/${name}.${mappedgenome}.markedDups.bam
+    mv ${sampleOutdir}/${name}.${mappedgenome}.markedDups.bam ${sampleOutdir}/${name}.${mappedgenome}.bam
 fi
 
 

dnase/submit.sh
@@ -14,6 +14,7 @@ module load FastQC/0.11.4
 module load bedtools/2.29.2
 module load bedops/2.4.37
 module load bwa/0.7.17
+module load minimap2/2.17
 module load htslib/1.10.2
 module load samtools/1.10
 module load bcftools/1.10.2
@@ -24,13 +25,14 @@ module load ucsckentutils/379
 module load hotspot/4.1
 module load hotspot2/2.1.1
 module load pigz
+module load primerclip/0.3.8
 
 
 ###Hardcoded configuration options
 #Common
 qsubargs=""
-mapThreads=4
-mergeThreads=3
+mapThreads=6
+mergeThreads=6
 runBamIntersect=1
 
 #For big jobs:
@@ -56,12 +58,12 @@ fi
 processingCommand=`echo "${analysisType}" | awk -F "," '{print $1}'`
 sampleType=`echo "${analysisType}" | awk -F "," '{print $2}'`
 
-if [[ "${processingCommand}" != "none" ]] && [[ "${processingCommand}" != "aggregate" ]] && [[ "${processingCommand}" != "aggregateRemarkDups" ]] && [[ "${processingCommand}" != "mapBwaAln" ]] && [[ "${processingCommand}" != "mapBwaMem" ]] && [[ "${processingCommand}" != "bamintersect" ]]; then
+if [[ "${processingCommand}" != "none" ]] && [[ "${processingCommand}" != "aggregate" ]] && [[ "${processingCommand}" != "aggregateRemarkDups" ]] && [[ "${processingCommand}" != "mapBwaAln" ]] && [[ "${processingCommand}" != "mapBwaMem" ]] && [[ "${processingCommand}" != "mapMinimap" ]] &&  [[ "${processingCommand}" != "bamintersect" ]]; then
     echo "ERROR submit: unknown processing command ${processingCommand} in analysisType ${analysisType}"
     exit 1
 fi
 
-if [[ "${sampleType}" != "atac" ]] && [[ "${sampleType}" != "dnase" ]] && [[ "${sampleType}" != "chipseq" ]] && [[ "${sampleType}" != "dna" ]] && [[ "${sampleType}" != "capture" ]] && [[ "${sampleType}" != "none" ]]; then
+if [[ "${sampleType}" != "atac" ]] && [[ "${sampleType}" != "dnase" ]] && [[ "${sampleType}" != "chipseq" ]] && [[ "${sampleType}" != "dna" ]] && [[ "${sampleType}" != "capture" ]] && [[ "${sampleType}" != "amplicon" ]] && [[ "${sampleType}" != "none" ]]; then
     echo "ERROR submit: unknown sample type ${sampleType} in analysisType ${analysisType}"
     exit 2
 fi

dnase/trackhub/MakeTrackhub.py
@@ -123,7 +123,7 @@ for line in inputfile_reader:
 # in the following for loop, as well as in the various track construction blocks below.
 assays = set([])
 for assay_type in set([line.get('Assay') for line in input_data_all]):
-    if assay_type in["DNase-seq", "DNA", "DNA Capture", "None", "ATAC-seq"]:
+    if assay_type in["DNase-seq", "DNA", "DNA Capture", "Amplicon", "None", "ATAC-seq"]:
         assays.add(assay_type)
     else:
         #ChIP-seq tracks have the epitope as the assay type
@@ -172,7 +172,7 @@ if args.supertrack is not None:
 # Now build the composites, and add them all to the supertrack.
 for assay_type in assays:
     #Matches the options allowed in submit.sh and createFlowcellSubmit.py
-    bwaPipelineAnalysisCommandMap = { "DNase-seq": "dnase", "Nano-DNase": "dnase", "ATAC-seq":"atac", "ChIP-seq": "chipseq", "DNA": "dna", "DNA Capture": "capture" }
+    bwaPipelineAnalysisCommandMap = { "DNase-seq": "dnase", "Nano-DNase": "dnase", "ATAC-seq":"atac", "ChIP-seq": "chipseq", "DNA": "dna", "DNA Capture": "capture", "Amplicon": "amplicon" }
     assay_suffix = bwaPipelineAnalysisCommandMap[assay_type] if assay_type in bwaPipelineAnalysisCommandMap else "none"
     
     input_data = []
@@ -403,7 +403,7 @@ for assay_type in assays:
             #Avoid making entries for tracks with no reads (since there are no .bw/.bb files); the bam file still shows up in case there is something there, and the samples still show up in the summary table at bottom in UCSC browser
             if curSample['analyzed_reads']!="NA" and int(curSample['analyzed_reads'])>0:
                 #Coverage_view
-                if assay_type in ["DNA", "DNA Capture"]:
+                if assay_type in ["DNA", "DNA Capture", "Amplicon"]:
                     if Coverage_view is None:
                         Coverage_view = ViewTrack(
                             name="Coverage_view_" + curGroup_trackname,
@@ -541,7 +541,7 @@ for assay_type in assays:
                     Cuts_view.add_tracks(track)
                 
                 #Variants_view
-                if assay_type in ["DNA", "DNA Capture"]:
+                if assay_type in ["DNA", "DNA Capture", "Amplicon"]:
                     if Variants_view is None:
                         Variants_view = ViewTrack(
                             name="Variants_view_" + curGroup_trackname,
@@ -567,7 +567,7 @@ for assay_type in assays:
                     Variants_view.add_tracks(track)
                 
                 #Genotypes_view
-                if assay_type in ["DNA", "DNA Capture"]:
+                if assay_type in ["DNA", "DNA Capture", "Amplicon"]:
                     if Genotypes_view is None:
                         Genotypes_view = ViewTrack(
                             name="Genotypes_view_" + curGroup_trackname,

dnase/trackhub/samplesforTrackhub.R
@@ -243,6 +243,8 @@ for(curdir in mappeddirs) {
 			data$Assay[i] <- "DNA"
 		} else if(sampleType=="capture" || sampleType=="callsnpsCapture") {
 			data$Assay[i] <- "DNA Capture"
+		} else if(sampleType=="amplicon") {
+			data$Assay[i] <- "Amplicon"
 		} else if(sampleType=="chipseq") {
 			data$Assay[i] <- SampleIDsplit[2]
 		} else {

flowcells/createFlowcellSubmit.py
@@ -200,8 +200,8 @@ def chromConfCapture(line):
 
 
 #BWA pipeline for DNA / DNase / ChIP-seq
-bwaPipelineAnalysisCommandMap = { "DNase-seq": "dnase", "Nano-DNase": "dnase", "ChIP-seq": "chipseq", "DNA": "dna", "DNA Capture": "capture" }
-bwaPipelineFragmentLengthsMap = { "DNase-seq": 300, "Nano-DNase": 300, "ChIP-seq": 500, "DNA": 750 , "DNA Capture": 750 }
+bwaPipelineAnalysisCommandMap = { "DNase-seq": "dnase", "Nano-DNase": "dnase", "ChIP-seq": "chipseq", "DNA": "dna", "DNA Capture": "capture", "Amplicon": "amplicon" }
+bwaPipelineFragmentLengthsMap = { "DNase-seq": 300, "Nano-DNase": 300, "ChIP-seq": 500, "DNA": 750 , "DNA Capture": 750, "Amplicon": 500 }
 
 
 def getBwaPipelineOutdir(sampleType):
@@ -255,7 +255,7 @@ def addCEGSgenomes(line):
 
 
 def getBwaReference(species):
-    if sampleType in ["DNA", "DNA Capture"]:
+    if sampleType in ["DNA", "DNA Capture", "Amplicon"]:
         speciesToGenomeReference = {
             "Human": "hg38_full",
             "Mouse": "mm10",
@@ -304,7 +304,7 @@ def bwaPipeline(line):
     elif args.aggregate_sublibraries:
         processingCommand="aggregateRemarkDups"
     else:
-        if sampleType in ["DNA", "DNA Capture"]:
+        if sampleType in ["DNA", "DNA Capture", "Amplicon"]:
             processingCommand="mapBwaMem"
         elif sampleType in ["Nano-DNase", "ChIP-seq", "DNase-seq"]:
             processingCommand="mapBwaAln"

commit 6382c095a53a601ed8bff3614f5ed068363e83de

fix mapBwaAln regression from 310ca5195daa75c95d54329a47d1a5b2318f8c61

dnase/map.sh
@@ -345,8 +345,8 @@ for curGenome in `echo ${genomesToMap} | perl -pe 's/,/ /g;'`; do
                 echo "Extracting unpaired reads"
                 unpairedReadsSam="$TMPDIR/${curfile}.${curGenome}.unpaired.sam"
                 unpairedExtractcmd="bwa samse ${bwaAlnExtractOpts} ${bwaIndex} $TMPDIR/${curfile}.unpaired.${curGenome}.sai $TMPDIR/${curfile}.unpaired.fastq.gz"
-                echo -e "unpairedExtractcmd=bwa $unpairedExtractcmd | (...)"
-                bwa $unpairedExtractcmd | grep -v "^@" > ${unpairedReadsSam}
+                echo -e "unpairedExtractcmd=${unpairedExtractcmd} | (...)"
+                ${unpairedExtractcmd} | grep -v "^@" > ${unpairedReadsSam}
                 
                 extractcmd="${extractcmd} | cat - ${unpairedReadsSam}"
             fi