c5894b63648bd3f8a9f344bb16cd873b47531f8d
mattmaurano <maurano@nyu.edu>
2021-09-01 21:49:19 -0400

    clarify column name in UCSC browser hub splash page

--- dnase/trackhub/makeChroms_per_cegsvectorHtml.R
+++ dnase/trackhub/makeChroms_per_cegsvectorHtml.R
@@ -9,10 +9,10 @@ fname_out <- args[2]
 # Get the text version of the table
 desc_text <- read.table(fname_in, sep="|", header=FALSE, stringsAsFactors=FALSE)
 
-colnames(desc_text) <- c("Reference", "Chromosomes")
+colnames(desc_text) <- c("Custom Reference", "Chromosomes")
 
 # Make the html version
 desc_HTML <- tableHTML(desc_text, rownames=FALSE, escape=FALSE) %>% add_css_row(css = list('background-color', 'lightblue'), rows = odd(1:(1+nrow(desc_text)))) %>%
-														add_css_row(css = list('background-color', 'white'), rows = even(1:(1+nrow(desc_text))))
+add_css_row(css = list('background-color', 'white'), rows = even(1:(1+nrow(desc_text))))
 write_tableHTML(desc_HTML, file=fname_out, complete_html=FALSE)
 
