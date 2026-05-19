mauranolab/mapping

commit 992f631a9bc84eb22f1263fd26b08e96d374cf29

Color genotypes bb lines (#250)
* Color genotype tracks (het - blue, hz_ref - grey, hz_nonref - yellow). fixes #215

dnase/trackhub/MakeTrackhub.py
@@ -584,8 +584,9 @@ for assay_type in assays:
                         long_label=assay_type + ' Genotypes ' + sampleDescription,
                         url=args.URLbase + urllib.parse.quote(curSample['filebase']) + '.genotypes.bb',
                         subgroups=sampleSubgroups,
-                        tracktype='bigBed 5',
+                        tracktype='bigBed 5 +',
                         color=curSample['Color'],
+                        itemRgb="on",
                         parentonoff="off"
                     )
                     Genotypes_view.add_tracks(track)

dnase/callsnpsMerge.sh
@@ -192,9 +192,26 @@ for sampleid in `bcftools query -l ${sampleOutdir}/${name}.${mappedgenome}.filte
     #Start from .txt file to simplify logic even though we have to sort again
     #If there is no database ID (e.g. rsid), set up bed ID as chrom : pos _ alt; for database IDs, append alt allele
     #truncgt() is a fancy wrapper around substr for syntactic sugar and to add a "..."
-    cat $TMPDIR/variants.${sampleid}.txt | awk -v maxlen=115 -F "\t" 'BEGIN {OFS="\t"} function truncgt(x) {if(length(x)>maxlen) {return substr(x, 1, maxlen) "..." } else {return x} } $4=="." {chromnum=$1; gsub(/^chr/, "", chromnum); $4=chromnum ":" $2+1 } {split($8, gt, "/"); gtout=truncgt(gt[1]); if(length(gt)>2) {gtout=gtout "/" truncgt(gt[2])}; $4=$4 "_" gtout } {print}' | sort-bed - | cut -f1-5 > $TMPDIR/${name}${vcfsamplename}.genotypes.ucsc.bed
-    bedToBigBed -type=bed5 $TMPDIR/${name}${vcfsamplename}.genotypes.ucsc.bed ${chromsizes} ${sampleOutdir}/${name}${vcfsamplename}.${mappedgenome}.genotypes.bb
-    
+    cat $TMPDIR/variants.${sampleid}.txt | awk -v maxlen=115 -F "\t" 'BEGIN {OFS="\t"} function truncgt(x) {if(length(x)>maxlen) {return substr(x, 1, maxlen) "..." } else {return x} } $4=="." {chromnum=$1; gsub(/^chr/, "", chromnum); $4=chromnum ":" $2+1 } {split($8, gt, "/"); gtout=truncgt(gt[1]); if(length(gt)>2) {gtout=gtout "/" truncgt(gt[2])}; $4=$4 "_" gtout } {print}' |
+    awk -F "\t" 'BEGIN {OFS="\t"} {
+        ref=$6;
+        numAlleles=split($8, gt, "/");
+        # Assign colors to the genotypes.bb lines.
+        if (numAlleles>1 && gt[1]!=gt[2]) {
+            color="19,165,220";        # het - blue
+        } else {
+            if (gt[1] == ref) {
+                color="105,105,105";   # hz_ref - grey
+            } else {
+                color="253,199,0";     # hz_nonref - yellow
+            }
+        }
+        print $1, $2, $3, $4, $5, ".", $2, $3, color;
+    }' |
+    sort-bed - > $TMPDIR/${name}${vcfsamplename}.genotypes.ucsc.bed
+
+    bedToBigBed -type=bed9 $TMPDIR/${name}${vcfsamplename}.genotypes.ucsc.bed ${chromsizes} ${sampleOutdir}/${name}${vcfsamplename}.${mappedgenome}.genotypes.bb
+
     #Personal Genome SNP format displays two alleles in vertical fashion and provides amino acid changes, but doesn't permit IDs
     #awk -F "\t" 'BEGIN {OFS="\t"} {split($8, gt, "/"); print $1, $2, $3, $8, length(gt), "0,0", "0,0"}'
     #https://genome.ucsc.edu/FAQ/FAQformat.html#format10

commit 12dbaa73c0c3bafb2679c9a1bd471c83392355c5

fix allelic genotypes color by removing trackDb color setting for Genotypes tracks (#285)
This was a longstanding bug in MakeTrackhub.py introduced in 992f631a9bc84eb22f1263fd26b08e96d374cf29 to fix #215 left color when adding itemRgb. The browser must have recently switched priority so genotype tracks lost variant-specific coloring

dnase/trackhub/MakeTrackhub.py
@@ -581,7 +581,6 @@ for assay_type in sorted(assays):
                         url=args.URLbase + urllib.parse.quote(curSample['filebase']) + '.genotypes.bb',
                         subgroups=sampleSubgroups,
                         tracktype='bigBed 9 .',
-                        color=curSample['Color'],
                         itemRgb="on",
                         parent="off"
                     )